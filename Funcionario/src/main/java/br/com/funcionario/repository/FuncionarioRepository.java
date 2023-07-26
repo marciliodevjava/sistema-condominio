54afe84c7fabf9cc3b9b0599eb61585cb3b57252
marciliodevjava <marciliodevjava@gmail.com>
2023-07-26 19:10:58 -0300

    Create FuncionarioRepository

---
+++ Funcionario/src/main/java/br/com/funcionario/repository/FuncionarioRepository.java
@@ -0,0 +1,9 @@
+package br.com.funcionario.repository;
+
+import br.com.funcionario.model.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+}
