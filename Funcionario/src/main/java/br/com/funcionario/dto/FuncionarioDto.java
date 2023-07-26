95924ce5a63eb4f5538f3192949bdbfb171f1143
marciliodevjava <marciliodevjava@gmail.com>
2023-07-26 19:10:50 -0300

    Create FuncionarioDto

---
+++ Funcionario/src/main/java/br/com/funcionario/dto/FuncionarioDto.java
@@ -0,0 +1,38 @@
+package br.com.funcionario.dto;
+
+import br.com.funcionario.model.Dependentes;
+import br.com.funcionario.model.Endereco;
+import br.com.funcionario.model.Funcionario;
+import br.com.funcionario.model.enuns.EstadoCivilEnum;
+import br.com.funcionario.model.enuns.EstadoEnum;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.NonNull;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.util.List;
+
+public record FuncionarioDto(
+        @NotNull(message = "Campo nome inválido.")
+        String nome,
+        @NotNull(message = "Campo cpf inválido.")
+        @CPF
+        String cpf,
+        @NotNull(message = "Campo rg inválido.")
+        String rg,
+        @NotNull(message = "Campo e-mail inválido.")
+        @Email
+        String email,
+        @NotNull(message = "Campo ddd inválido.")
+        String ddd,
+        @NotNull(message = "Campo telefone inválido.")
+        String telefone,
+        @NotBlank(message = "Campo situacao inválido.")
+        EstadoEnum situacao,
+        @NotBlank(message = "Campo estadoCivil inválio.")
+        EstadoCivilEnum estadoCivil,
+        List<DependentesDto> dependentes,
+        EnderecoDto endereco) {
+
+}
