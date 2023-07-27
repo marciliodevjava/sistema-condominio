3bb4fe214baa51849dfaca6a0b097a77e1ded9f2
marciliodevjava <marciliodevjava@gmail.com>
2023-07-27 17:54:16 -0300

    Create GeradorUuid

---
+++ Gerador/src/main/java/br/com/gerador/utils/GeradorUuid.java
@@ -0,0 +1,43 @@
+package br.com.gerador.utils;
+
+import br.com.gerador.domain.Uuid;
+import br.com.gerador.domain.enuns.EnumProjeto;
+import br.com.gerador.repository.UuidRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+public class GeradorUuid {
+
+    @Autowired
+    private UuidRepository uuidRepository;
+
+    public String getIdentificadorUuid() {
+        UUID identificador;
+        Uuid uuid;
+        String uuidGerado;
+
+        do {
+            identificador = UUID.randomUUID();
+            uuid = uuidRepository.findByUuidGerado(identificador);
+        } while (uuid != null);
+
+        uuidGerado = String.valueOf(identificador);
+
+        Uuid salvar = new Uuid();
+
+        salvar.setUuidGerado(identificador);
+        salvar.setData(LocalDate.now());
+        salvar.setHora(LocalTime.now());
+        salvar.setProjeto(EnumProjeto.GERADOR);
+
+        salvar = uuidRepository.save(salvar);
+
+        return uuidGerado.toString();
+    }
+}
