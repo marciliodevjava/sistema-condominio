27ff41a4f29f33beed72ac596e8e44d82aa8ef4a
marciliodevjava <marciliodevjava@gmail.com>
2023-07-27 17:55:09 -0300

    Create FuncionarioApplication

--- Funcionario/src/main/java/br/com/funcionario/FuncionarioApplication.java
+++ Funcionario/src/main/java/br/com/funcionario/FuncionarioApplication.java
@@ -2,8 +2,10 @@ package br.com.funcionario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class FuncionarioApplication {
 
 	public static void main(String[] args) {
