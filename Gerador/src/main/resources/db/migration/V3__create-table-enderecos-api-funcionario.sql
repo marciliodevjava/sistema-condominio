f4ed61a8610548e037c5b72766307a4352b10ab3
marciliodevjava <marciliodevjava@gmail.com>
2023-07-26 23:43:58 -0300

    Update V3__create-table-enderecos-api-funcionario.sql

--- Gerador/src/main/resources/db/migration/V3__create-table-enderecos-api-funcionario.sql
+++ Gerador/src/main/resources/db/migration/V3__create-table-enderecos-api-funcionario.sql
@@ -1,15 +1,16 @@
 CREATE TABLE tb_enderecos
 (
-    id           bigint(20) not null AUTO_INCREMENT,
-    cep        varchar(8) not null,
-    logradouro          varchar(255)  not null,
-    numero          varchar(10)   not null,
-    bairro     varchar(100),
-    cidade     varchar(100)  not null,
-    uf     varchar(15)  not null,
-    pais     varchar(161)  not null,
-    id_funcionario bigint(20),
+    id                 bigint(20)   not null AUTO_INCREMENT,
+    uuid_identificador varchar(36)  not null,
+    cep                varchar(8)   not null,
+    logradouro         varchar(255) not null,
+    numero             varchar(10)  not null,
+    bairro             varchar(100),
+    cidade             varchar(100) not null,
+    uf                 varchar(15)  not null,
+    pais               varchar(161) not null,
+    id_funcionario     bigint(20),
 
     PRIMARY KEY (id),
-    FOREIGN KEY (id_funcionario) REFERENCES tb_funcionarios(id)
+    FOREIGN KEY (id_funcionario) REFERENCES tb_funcionarios (id)
 );
