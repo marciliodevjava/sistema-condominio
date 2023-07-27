d528940d0a4afe1a5534ac2f592004908503deb2
marciliodevjava <marciliodevjava@gmail.com>
2023-07-26 23:42:26 -0300

    Update Dependentes

--- Funcionario/src/main/java/br/com/funcionario/model/Dependentes.java
+++ Funcionario/src/main/java/br/com/funcionario/model/Dependentes.java
@@ -2,13 +2,12 @@ package br.com.funcionario.model;
 
 import br.com.funcionario.model.enuns.GrauParentescoEnum;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
 
 @Entity
 @Table(name = "tb_dependentes")
@@ -16,17 +15,22 @@ import java.io.Serializable;
 @AllArgsConstructor
 @Getter
 @Setter
+@EqualsAndHashCode
 public class Dependentes implements Serializable {
     @Serial
-    private static final long serialVersionUID = 6L;
+    private static final long serialVersionUID = 3L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "uuid_identificador", length = 36)
+    private UUID uuidIdentificador;
     @Enumerated(EnumType.STRING)
     @Column(name = "grau_parentesco", length = 13)
     private GrauParentescoEnum grauParentesco;
     @Column(name = "nome", length = 150)
     private String nome;
+    @Column(name = "data_nascimento")
+    private Date dataNascimento;
     @Column(name = "cpf", length = 11)
     private String cpf;
     @Column(name = "rg", length = 11)
