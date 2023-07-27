5634da0b33050fca8f5d37489ae48f5f1063b345
marciliodevjava <marciliodevjava@gmail.com>
2023-07-26 23:43:28 -0300

    Update V1__create-table-funcionario-api-funcionario.sql

--- Gerador/src/main/resources/db/migration/V1__create-table-funcionario-api-funcionario.sql
+++ Gerador/src/main/resources/db/migration/V1__create-table-funcionario-api-funcionario.sql
@@ -1,15 +1,17 @@
 CREATE TABLE tb_funcionarios
 (
-    id           bigint(20) not null AUTO_INCREMENT,
+    id                 bigint(20) not null AUTO_INCREMENT,
+    uuid_identificador varchar(36)  not null,
     numero_funcionario bigint(20),
-    nome         varchar(255) not null,
-    cpf          varchar(11)  not null,
-    rg          varchar(10)  not null,
-    email        varchar(150) not null,
-    ddd          varchar(3)   not null,
-    telefone     varchar(10)  not null,
-    situacao     varchar(9)   not null,
-    estado_civil varchar(10)  not null,
+    nome               varchar(255) not null,
+    data_nascimento    date,
+    cpf                varchar(11)  not null,
+    rg                 varchar(10)  not null,
+    email              varchar(150) not null,
+    ddd                varchar(3)   not null,
+    telefone           varchar(10)  not null,
+    situacao           varchar(9)   not null,
+    estado_civil       varchar(10)  not null,
 
-    primary key(id)
+    primary key (id)
 );
