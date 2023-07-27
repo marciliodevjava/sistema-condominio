b1c42014ef5e045bc96afa13cb320778aed7010f
marciliodevjava <marciliodevjava@gmail.com>
2023-07-27 17:52:53 -0300

    Create MatriculaFuncionarioService

---
+++ Gerador/src/main/java/br/com/gerador/service/MatriculaFuncionarioService.java
@@ -0,0 +1,27 @@
+package br.com.gerador.service;
+
+import br.com.gerador.domain.MatriculaFuncionario;
+import br.com.gerador.dto.MatriculaFuncionarioDto;
+import br.com.gerador.mapper.MatriculaFuncionarioMapper;
+import br.com.gerador.repository.MatriculaFuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class MatriculaFuncionarioService {
+
+    @Autowired
+    private MatriculaFuncionarioMapper matriculaFuncionarioMapper;
+
+    public MatriculaFuncionarioDto criarMatriculaFuncionarioService(Long id) {
+
+        MatriculaFuncionario funcionario = new MatriculaFuncionario();
+
+        funcionario = matriculaFuncionarioMapper.gerarFuncionario(id);
+
+
+        return null;
+    }
+}
