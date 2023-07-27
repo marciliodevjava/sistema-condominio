602ad1bdffb4ee45a3d4829788934d31565b7d35
marciliodevjava <marciliodevjava@gmail.com>
2023-07-26 23:43:42 -0300

    Update V2__create-table-dependentes-api-funcionario.sql

--- Gerador/src/main/resources/db/migration/V2__create-table-dependentes-api-funcionario.sql
+++ Gerador/src/main/resources/db/migration/V2__create-table-dependentes-api-funcionario.sql
@@ -1,14 +1,16 @@
 CREATE TABLE tb_dependentes
 (
-    id           bigint(20) not null AUTO_INCREMENT,
-    grau_parentesco         varchar(13) not null,
-    nome        varchar(150) not null,
-    cpf          varchar(11)  not null,
-    rg          varchar(10)   not null,
-    ddd     varchar(3),
-    telefone     varchar(10)  not null,
-    id_funcionario bigint(20),
+    id                 bigint(20)   not null AUTO_INCREMENT,
+    uuid_identificador varchar(36)  not null,
+    grau_parentesco    varchar(13)  not null,
+    nome               varchar(150) not null,
+    data_nascimento    date,
+    cpf                varchar(11)  not null,
+    rg                 varchar(10)  not null,
+    ddd                varchar(3),
+    telefone           varchar(10)  not null,
+    id_funcionario     bigint(20),
 
     PRIMARY KEY (id),
-    FOREIGN KEY (id_funcionario) REFERENCES tb_funcionarios(id)
+    FOREIGN KEY (id_funcionario) REFERENCES tb_funcionarios (id)
 );
