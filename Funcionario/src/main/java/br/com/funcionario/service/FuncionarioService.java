624dc4a2654fccf65cda02779b1306a276285088
marciliodevjava <marciliodevjava@gmail.com>
2023-07-26 23:43:12 -0300

    Update FuncionarioService

--- Funcionario/src/main/java/br/com/funcionario/service/FuncionarioService.java
+++ Funcionario/src/main/java/br/com/funcionario/service/FuncionarioService.java
@@ -2,6 +2,8 @@ package br.com.funcionario.service;
 
 import br.com.funcionario.dto.FuncionarioDto;
 import br.com.funcionario.dto.FuncionarioRetornoDto;
+import br.com.funcionario.mapper.FuncionarioMapper;
+import br.com.funcionario.model.Funcionario;
 import br.com.funcionario.repository.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +14,11 @@ public class FuncionarioService {
     @Autowired
     private FuncionarioRepository funcionarioRepository;
 
+    @Autowired
+    private FuncionarioMapper funcionarioMapper;
+
     public FuncionarioRetornoDto salvarFuncionario(FuncionarioDto dto) {
+        Funcionario funcionario = funcionarioMapper.mapear(dto);
         return null;
     }
 }
