a6f0f2ac9fa161c230f9ec055ecc3711faa43b9c
marciliodevjava <marciliodevjava@gmail.com>
2023-07-31 23:10:45 -0300

    Update MatriculaFuncionarioResource

--- Gerador/src/main/java/br/com/gerador/resource/MatriculaFuncionarioResource.java
+++ Gerador/src/main/java/br/com/gerador/resource/MatriculaFuncionarioResource.java
@@ -1,9 +1,9 @@
 package br.com.gerador.resource;
 
 import br.com.gerador.dto.MatriculaFuncionarioDto;
+import br.com.gerador.dto.MatriculaFuncionarioUtualizarDto;
 import br.com.gerador.service.MatriculaFuncionarioService;
 import jakarta.validation.constraints.NotNull;
-import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,15 +19,23 @@ public class MatriculaFuncionarioResource {
     private MatriculaFuncionarioService funcionarioService;
 
     @PostMapping("/{id}")
-    public ResponseEntity<MatriculaFuncionarioDto> gerarMatricula(@PathVariable @NotNull Long id, UriComponentsBuilder uri){
+    public ResponseEntity<MatriculaFuncionarioDto> gerarMatricula(@PathVariable @NotNull Long id, UriComponentsBuilder uri) {
         MatriculaFuncionarioDto matriculaFuncionario = funcionarioService.criarMatriculaFuncionarioService(id);
         URI retorno = uri.path("/gerador/{id}").buildAndExpand(matriculaFuncionario.getId()).toUri();
         return ResponseEntity.created(retorno).body(matriculaFuncionario);
     }
 
-    @GetMapping("{id}")
-    public ResponseEntity<MatriculaFuncionarioDto> getMatricula(@PathVariable Long id){
+    @GetMapping("/{id}")
+    public ResponseEntity<MatriculaFuncionarioDto> getMatricula(@PathVariable Long id) {
         MatriculaFuncionarioDto matriculaFuncionario = funcionarioService.buscarFuncionarioPorId(id);
         return ResponseEntity.ok(matriculaFuncionario);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MatriculaFuncionarioDto> atualizarMatricula(@PathVariable Long id,
+                                                                      @RequestBody MatriculaFuncionarioUtualizarDto funcionarioAtualizarDto) {
+
+        MatriculaFuncionarioDto atualizarMatricula = funcionarioService.atualizarMatricula(id, funcionarioAtualizarDto);
+        return ResponseEntity.ok(atualizarMatricula);
     }
 }
