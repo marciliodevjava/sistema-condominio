4b7981727ad62a64101b173abcde557319e6849b
marciliodevjava <marciliodevjava@gmail.com>
2023-08-01 17:23:35 -0300

    Update FormatadorDeDados

--- Funcionario/src/main/java/br/com/funcionario/utils/FormatadorDeDados.java
+++ Funcionario/src/main/java/br/com/funcionario/utils/FormatadorDeDados.java
@@ -4,4 +4,37 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class FormatadorDeDados {
+
+    public String formatadorCpf(String cpf){
+
+        cpf = cpf.replace(" ","");
+        cpf = cpf.replace(".","");
+        cpf = cpf.replace("-","");
+
+        return cpf.trim();
+    }
+
+    public String formatadorNome(String nome) {
+        nome = nome.replace(" ","");
+        return nome.trim();
+    }
+
+    public String formatadorRg(String rg) {
+        rg = rg.replace(" ","");
+        rg = rg.replace(".","");
+        rg = rg.replace("-","");
+        return rg.trim();
+    }
+
+    public String formatadorEmail(String email) {
+        email = email.replace(" ","");
+        email = email.toLowerCase();
+        return email.trim();
+    }
+
+    public String formatadorTelefone(String telefone) {
+        telefone = telefone.replace("","");
+        telefone = telefone.replace("-","");
+        return telefone.trim();
+    }
 }
