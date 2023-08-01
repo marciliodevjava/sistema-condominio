e9755d59f68185f17c83b3138227d59cddb63bca
marciliodevjava <marciliodevjava@gmail.com>
2023-08-01 09:29:15 -0300

    Update FuncionarioService

--- Funcionario/src/main/java/br/com/funcionario/service/FuncionarioService.java
+++ Funcionario/src/main/java/br/com/funcionario/service/FuncionarioService.java
@@ -2,9 +2,12 @@ package br.com.funcionario.service;
 
 import br.com.funcionario.dto.FuncionarioDto;
 import br.com.funcionario.dto.FuncionarioRetornoDto;
+import br.com.funcionario.dto.NumeroDto;
+import br.com.funcionario.http.GeradorClients;
 import br.com.funcionario.mapper.FuncionarioMapper;
 import br.com.funcionario.model.Funcionario;
 import br.com.funcionario.repository.FuncionarioRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +20,23 @@ public class FuncionarioService {
     @Autowired
     private FuncionarioMapper funcionarioMapper;
 
+    @Autowired
+    private GeradorClients geradorClients;
+    @Autowired
+    private ObjectMapper objectMapper;
+
     public FuncionarioRetornoDto salvarFuncionario(FuncionarioDto dto) {
         Funcionario funcionario = funcionarioMapper.mapear(dto);
-        return null;
+        funcionario = funcionarioRepository.save(funcionario);
+        funcionario.setNumeroFuncionario(gerarNumeroFuncionario(funcionario.getId()));
+        funcionario = funcionarioRepository.save(funcionario);
+        return objectMapper.convertValue(funcionario, FuncionarioRetornoDto.class);
+    }
+
+    private Integer gerarNumeroFuncionario(Long id) {
+
+        NumeroDto numero = geradorClients.gerarFuncionario(id);
+
+        return numero.getNumeroFuncionario();
     }
 }
