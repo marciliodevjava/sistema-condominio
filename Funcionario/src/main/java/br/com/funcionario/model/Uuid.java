482ac3455de5a87db702b261bee346840d436710
marciliodevjava <marciliodevjava@gmail.com>
2023-08-01 09:29:59 -0300

    Create Uuid

---
+++ Funcionario/src/main/java/br/com/funcionario/model/Uuid.java
@@ -0,0 +1,35 @@
+package br.com.funcionario.model;
+
+import br.com.funcionario.model.enuns.EnumProjeto;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "tb_uuid")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Uuid implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 6L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "uuid_gerador")
+    private String uuidGerado;
+    @Column(name = "data")
+    private LocalDate data;
+    @Column(name = "hora")
+    private LocalTime hora;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "projeto")
+    private EnumProjeto projeto;
+}
