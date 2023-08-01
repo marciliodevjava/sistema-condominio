a89d79740911d3c11a94235e14c54ea91f6e38fa
marciliodevjava <marciliodevjava@gmail.com>
2023-08-01 09:29:25 -0300

    Update FuncionarioRetornoDto

--- Funcionario/src/main/java/br/com/funcionario/dto/FuncionarioRetornoDto.java
+++ Funcionario/src/main/java/br/com/funcionario/dto/FuncionarioRetornoDto.java
@@ -1,9 +1,29 @@
 package br.com.funcionario.dto;
 
-public record FuncionarioRetornoDto(Long id) {
+import br.com.funcionario.model.enuns.EstadoCivilEnum;
+import br.com.funcionario.model.enuns.EstadoEnum;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.List;
 
-    public Long getId() {
-        return id;
-    }
+@Getter
+@Setter
+public class FuncionarioRetornoDto {
+
+    private Long id;
+
+    private String uuidIdentificador;
+    private Integer numeroFuncionario;
+    private String nome;
+    private String cpf;
+    private String rg;
+    private String email;
+    private String ddd;
+    private String telefone;
+    private EstadoEnum situacao;
+    private EstadoCivilEnum estadoCivil;
+    private List<DependentesDto> dependentes;
+    private EnderecoDto endereco;
 }
+
