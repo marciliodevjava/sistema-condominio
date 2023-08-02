a069b7df44cb90be9a0f5d4b211ffc4737d5b977
marciliodevjava <marciliodevjava@gmail.com>
2023-08-02 09:31:08 -0300

    Create LoggerUltis

---
+++ Gerador/src/main/java/br/com/gerador/utils/LoggerUltis.java
@@ -0,0 +1,28 @@
+package br.com.gerador.utils;
+
+import net.logstash.logback.argument.StructuredArguments;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoggerUltis {
+
+    private static final Logger logger = LogManager.getLogger(LoggerUltis.class);
+
+    LoggerUltis() {
+    }
+
+
+    public void logInfo(String mensagem) {
+        logger.info("{}", mensagem);
+    }
+
+    public void logInfo(String mensagem, String identificador) {
+        logger.info("{} {}", mensagem, StructuredArguments.value("identificador", identificador));
+    }
+
+    public void logInfo(String mensagem, String identificador, String localizador) {
+        logger.info("{} {} {}", mensagem, StructuredArguments.value("identificador", identificador), StructuredArguments.value("localizador", localizador));
+    }
+}
