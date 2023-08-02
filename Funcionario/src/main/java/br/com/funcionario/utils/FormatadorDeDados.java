595ee74c2fc66df6d8dd92b28125395dc72accea
marciliodevjava <marciliodevjava@gmail.com>
2023-08-02 19:06:06 -0300

    Update FormatadorDeDados

--- Funcionario/src/main/java/br/com/funcionario/utils/FormatadorDeDados.java
+++ Funcionario/src/main/java/br/com/funcionario/utils/FormatadorDeDados.java
@@ -2,39 +2,51 @@ package br.com.funcionario.utils;
 
 import org.springframework.stereotype.Component;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.SimpleFormatter;
+
 @Component
 public class FormatadorDeDados {
 
-    public String formatadorCpf(String cpf){
+    public String formatadorCpf(String cpf) {
 
-        cpf = cpf.replace(" ","");
-        cpf = cpf.replace(".","");
-        cpf = cpf.replace("-","");
+        cpf = cpf.replace(" ", "");
+        cpf = cpf.replace(".", "");
+        cpf = cpf.replace("-", "");
 
         return cpf.trim();
     }
 
     public String formatadorNome(String nome) {
-        nome = nome.replace(" ","");
         return nome.trim();
     }
 
     public String formatadorRg(String rg) {
-        rg = rg.replace(" ","");
-        rg = rg.replace(".","");
-        rg = rg.replace("-","");
+        rg = rg.replace(" ", "");
+        rg = rg.replace(".", "");
+        rg = rg.replace("-", "");
         return rg.trim();
     }
 
     public String formatadorEmail(String email) {
-        email = email.replace(" ","");
+        email = email.replace(" ", "");
         email = email.toLowerCase();
         return email.trim();
     }
 
     public String formatadorTelefone(String telefone) {
-        telefone = telefone.replace("","");
-        telefone = telefone.replace("-","");
+        telefone = telefone.replace("", "");
+        telefone = telefone.replace("-", "");
         return telefone.trim();
+    }
+
+    public Date formatadorData(String dataNascimento) throws ParseException {
+        String dataFormatar = "yyyy-MM-dd";
+        SimpleDateFormat formatarData = new SimpleDateFormat(dataFormatar);
+        Date data = formatarData.parse(dataNascimento);
+
+        return data;
     }
 }
