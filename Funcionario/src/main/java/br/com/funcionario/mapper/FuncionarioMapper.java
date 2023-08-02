c928fc346a50ef05072443d78b123c82ac5b0a4a
marciliodevjava <marciliodevjava@gmail.com>
2023-08-02 19:06:59 -0300

    Update FuncionarioMapper

--- Funcionario/src/main/java/br/com/funcionario/mapper/FuncionarioMapper.java
+++ Funcionario/src/main/java/br/com/funcionario/mapper/FuncionarioMapper.java
@@ -1,8 +1,6 @@
 package br.com.funcionario.mapper;
 
 import br.com.funcionario.dto.FuncionarioDto;
-import br.com.funcionario.dto.NumeroDto;
-import br.com.funcionario.http.GeradorClients;
 import br.com.funcionario.model.Funcionario;
 import br.com.funcionario.model.enuns.EstadoEnum;
 import br.com.funcionario.utils.FormatadorDeDados;
@@ -10,6 +8,7 @@ import br.com.funcionario.utils.GeradorUuid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.text.ParseException;
 import java.util.Objects;
 
 @Component
@@ -18,13 +17,14 @@ public class FuncionarioMapper {
     private FormatadorDeDados formatadorDeDados;
     @Autowired
     private GeradorUuid geradorUuid;
-    public Funcionario mapear(FuncionarioDto dto) {
+    public Funcionario mapear(FuncionarioDto dto) throws ParseException {
         Funcionario funcionario = new Funcionario();
         if (Objects.nonNull(dto.getNome())){
             funcionario.setUuidIdentificador(geradorUuid.getIdentificadorUuid());
             funcionario.setNome(formatadorDeDados.formatadorNome(dto.getNome()));
             funcionario.setCpf(formatadorDeDados.formatadorCpf(dto.getCpf()));
             funcionario.setRg(formatadorDeDados.formatadorRg(dto.getRg()));
+            funcionario.setDataNascimento(formatadorDeDados.formatadorData(dto.getDataNascimento()));
             funcionario.setEmail(formatadorDeDados.formatadorEmail(dto.getEmail()));
             funcionario.setDdd(formatadorDeDados.formatadorNome(dto.getDdd()));
             funcionario.setTelefone(formatadorDeDados.formatadorTelefone(dto.getTelefone()));
