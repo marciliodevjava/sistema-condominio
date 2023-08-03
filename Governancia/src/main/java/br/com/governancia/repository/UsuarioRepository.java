ab933801debbaeae6df236bef83cf425bcf5adb1
marciliodevjava <marciliodevjava@gmail.com>
2023-08-03 16:24:37 -0300

    Create UsuarioRepository

---
+++ Governancia/src/main/java/br/com/governancia/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package br.com.governancia.repository;
+
+import br.com.governancia.usuario.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String login);
+}
+
