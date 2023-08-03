a5758ef0d7736dc93ed3e4d3a1e9ab50de062037
marciliodevjava <marciliodevjava@gmail.com>
2023-08-03 13:38:24 -0300

    Create V7__insert-into-root-usuario.sql

---
+++ Gerador/src/main/resources/db/migration/V7__insert-into-root-usuario.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_usuarios (id, nome, login_usuario, senha_usuario, uuid_identificador, data, hora)
+VALUES (1,
+        'root',
+        'root',
+        '$2a$10$sF6jVirJuRMAqzZfW1lGAe.tIcWo3oGAqZA.iI1VnJkzpD8mp13Sm',
+        '7b957075-7893-48d6-9cee-5a43115040d',
+        '2023-08-03',
+        '12:38:11');
