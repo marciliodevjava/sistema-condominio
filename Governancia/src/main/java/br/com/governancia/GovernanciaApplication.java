17a59f9e33f1385798199bd0b16e7549b7612370
marciliodevjava <marciliodevjava@gmail.com>
2023-08-03 09:28:59 -0300

    Create Api Governancia

---
+++ Governancia/src/main/java/br/com/governancia/GovernanciaApplication.java
@@ -0,0 +1,13 @@
+package br.com.governancia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovernanciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovernanciaApplication.class, args);
+	}
+
+}
---
+++ Governancia/src/test/java/br/com/governancia/GovernanciaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.governancia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovernanciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
