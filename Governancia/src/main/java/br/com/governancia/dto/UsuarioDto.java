d01c313de9ce7f6e93e4ca0fcb9827ba3c898b16
marciliodevjava <marciliodevjava@gmail.com>
2023-08-03 16:24:29 -0300

    Create UsuarioDto

---
+++ Governancia/src/main/java/br/com/governancia/dto/UsuarioDto.java
@@ -0,0 +1,14 @@
+package br.com.governancia.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UsuarioDto {
+    private String nome;
+    private String login;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String senha;
+}
