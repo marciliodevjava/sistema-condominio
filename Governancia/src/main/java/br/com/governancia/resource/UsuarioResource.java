ce948679c62f852ed1881b83da98149cffa5631f
marciliodevjava <marciliodevjava@gmail.com>
2023-08-04 19:19:38 -0300

    Update UsuarioRepository

--- Governancia/src/main/java/br/com/governancia/resource/UsuarioResource.java
+++ Governancia/src/main/java/br/com/governancia/resource/UsuarioResource.java
@@ -1,15 +1,15 @@
 package br.com.governancia.resource;
 
-import br.com.governancia.dto.DadosAutenticacaoDto;
-import br.com.governancia.dto.DadosTokenJWT;
-import br.com.governancia.dto.UsuarioAlteradoDto;
-import br.com.governancia.dto.UsuarioDto;
-import br.com.governancia.infra.exception.UsuarioNaoExisteException;
+import br.com.governancia.dto.*;
+import br.com.governancia.infra.exception.exception.UsuarioNaoExisteException;
 import br.com.governancia.infra.security.TokenService;
 import br.com.governancia.service.UsuarioService;
 import br.com.governancia.usuario.Usuario;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -32,7 +32,7 @@ public class UsuarioResource {
     private TokenService tokenService;
 
     @PostMapping
-    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacaoDto dados){
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacaoDto dados) {
         UsernamePasswordAuthenticationToken Autenticationtoken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         Authentication authentication = manager.authenticate(Autenticationtoken);
 
@@ -43,24 +43,38 @@ public class UsuarioResource {
         return ResponseEntity.ok(new DadosTokenJWT(dados.login(), tokenJWT, dataInicial, dataFinal));
     }
 
+
     @PostMapping("/cadastrar")
-    public ResponseEntity<UsuarioDto> cadastrar(@RequestBody @Valid UsuarioDto usuario){
+    public ResponseEntity<UsuarioDto> cadastrar(@RequestBody @Valid UsuarioDto usuario) {
         UsuarioDto retorno = usuarioService.crearUsuario(usuario);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(retorno);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioIdDto> buscarUsuarios(@PathVariable Long id) {
+        UsuarioIdDto usuario = usuarioService.buscarUsuarioId(id);
+
+        return ResponseEntity.ok(usuario);
+    }
+
+    @GetMapping("/todos")
+    public ResponseEntity<Page<UsuarioListaDto>> listarUsuarios(@PageableDefault(size = 10, page = 0, sort = "nome") Pageable paginacao) {
+        Page<UsuarioListaDto> listasPessoas = usuarioService.obterTodos(paginacao);
+
+        return ResponseEntity.ok(listasPessoas);
+    }
+
     @PutMapping("/alterar")
-    public ResponseEntity<?> alterar(@RequestBody @Valid UsuarioDto usuario){
+    public ResponseEntity<?> alterar(@RequestBody @Valid UsuarioDto usuario) {
         UsuarioAlteradoDto usuarioAlteradoDto = usuarioService.alterarUsuario(usuario);
 
-        if(Objects.nonNull(usuarioAlteradoDto.getNome())){
+        if (Objects.nonNull(usuarioAlteradoDto.getNome())) {
             return ResponseEntity.ok(usuarioAlteradoDto);
         }
-
-        try{
+        try {
             throw new UsuarioNaoExisteException();
-        } catch (UsuarioNaoExisteException ex){
+        } catch (UsuarioNaoExisteException ex) {
             System.out.println("Solicitação negada!");
         }
 
