379220c24d5c9f297659e5d5c6edb2a5bd5ca895
marciliodevjava <marciliodevjava@gmail.com>
2023-08-03 22:12:37 -0300

    Update UsuarioService

--- Governancia/src/main/java/br/com/governancia/service/UsuarioService.java
+++ Governancia/src/main/java/br/com/governancia/service/UsuarioService.java
@@ -1,15 +1,18 @@
 package br.com.governancia.service;
 
+import br.com.governancia.dto.UsuarioAlteradoDto;
 import br.com.governancia.dto.UsuarioDto;
 import br.com.governancia.repository.UsuarioRepository;
 import br.com.governancia.usuario.Usuario;
 import br.com.governancia.utils.GeradorUuid;
 import br.com.governancia.utils.HashUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalTime;
 import java.util.Date;
+import java.util.Objects;
 
 @Service
 public class UsuarioService {
@@ -18,6 +21,8 @@ public class UsuarioService {
     private GeradorUuid geradorUuid;
     @Autowired
     private UsuarioRepository usuarioRepository;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     public UsuarioDto crearUsuario(UsuarioDto usuario) {
         Usuario criarUsuario = this.montarUsuario(usuario);
@@ -49,5 +54,27 @@ public class UsuarioService {
         criarUsuario.setHora(LocalTime.now());
 
         return criarUsuario;
+    }
+
+    public UsuarioAlteradoDto alterarUsuario(UsuarioDto usuario) {
+        Usuario consultaUsuario = usuarioRepository.findByLoginAndNome(usuario.getLogin(), usuario.getNome());
+        if(Objects.nonNull(consultaUsuario)){
+            consultaUsuario = this.mapearUsuarioParaModificacao(consultaUsuario, usuario);
+            consultaUsuario = usuarioRepository.save(consultaUsuario);
+
+            return objectMapper.convertValue(consultaUsuario, UsuarioAlteradoDto.class);
+        }
+
+        return new UsuarioAlteradoDto();
+    }
+
+    private Usuario mapearUsuarioParaModificacao(Usuario consultaUsuario, UsuarioDto usuario) {
+
+        String senha = HashUtil.getSecureHash(usuario.getSenha());
+
+        consultaUsuario.setNome(usuario.getNome());
+        consultaUsuario.setSenha(senha);
+
+        return consultaUsuario;
     }
 }
