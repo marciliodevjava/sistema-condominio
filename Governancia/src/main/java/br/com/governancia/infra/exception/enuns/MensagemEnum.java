5d28ef49cddcd3b3b5f112f9e380d1cc1d1a6557
marciliodevjava <marciliodevjava@gmail.com>
2023-08-04 19:53:41 -0300

    Update MensagemEnum

--- Governancia/src/main/java/br/com/governancia/infra/exception/enuns/MensagemEnum.java
+++ Governancia/src/main/java/br/com/governancia/infra/exception/enuns/MensagemEnum.java
@@ -2,6 +2,7 @@ package br.com.governancia.infra.exception.enuns;
 
 public enum MensagemEnum {
     ERROR_ENTITY_NOT_FOUND("Não existe na base de dados o campo informado."),
+    ERROR_TOKEN("Token JWT inválido ou expirado."),
     ERRO_NO_SUCH_ELEMENT("Dado informado não encontrado."),
     ERRO_METHOD_ARGUMENT_NOT_VALID("Occoreu um erro na validação dos paramtros."),
     ERRO_HTTP_MESSAGE_NOT_REABLE("Erro 3: Acesso não autorizado."),
@@ -9,7 +10,8 @@ public enum MensagemEnum {
     ERRO_ACCESS_DENIED("Acesso negado."),
     ERRO_AUTHENTICATION("Credenciais inválidas."),
     ERRO_EXCEPTION("Erro: "),
-    ERRO_REQUISICAO_NEGADA("Solicitação negada.");
+    ERRO_REQUISICAO_NEGADA("Solicitação negada."),
+    ERRO_REQUISICAO_USUARIO_NAO_EXISTE("Usuario informado não existe, ou as informações estão erradas.");
 
     private final String mensagem;
 
