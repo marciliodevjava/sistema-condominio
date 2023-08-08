033757e442013ffb0d6023346fdf91efe3687f59
marciliodevjava <marciliodevjava@gmail.com>
2023-08-08 16:59:42 -0300

    Update FuncionarioService

--- Funcionario/src/main/java/br/com/funcionario/service/FuncionarioService.java
+++ Funcionario/src/main/java/br/com/funcionario/service/FuncionarioService.java
@@ -10,11 +10,16 @@ import br.com.funcionario.model.Funcionario;
 import br.com.funcionario.repository.FuncionarioRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.text.ParseException;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class FuncionarioService {
@@ -38,6 +43,11 @@ public class FuncionarioService {
         return objectMapper.convertValue(funcionario, FuncionarioRetornoDto.class);
     }
 
+    public FuncionarioRetornoDto salvarFuncionarioCompleto(FuncionarioDto dto) throws ParseException {
+        Funcionario funcionario = funcionarioMapper.mapear(dto);
+        return null;
+    }
+
     private Integer gerarNumeroFuncionario(Long id) {
 
         NumeroDto numero = geradorClients.gerarFuncionario(id);
@@ -59,5 +69,33 @@ public class FuncionarioService {
             return new FuncionarioDeletadoDto(id, "Funcionario Deletado com Sucesso!");
         }
         return new FuncionarioDeletadoDto(id, "Funcionario não deltado!");
+    }
+
+    public Page<FuncionarioRetornoDto> obterTodos(Pageable paginacao) {
+        Page<Funcionario> funcionario = funcionarioRepository.findAll(paginacao);
+        List<FuncionarioRetornoDto> funcionarioDto = funcionario.getContent().stream()
+                .map(this::mapFuncionarioRetornoDto)
+                .collect(Collectors.toList());
+
+        return new PageImpl<>(funcionarioDto, paginacao, funcionario.getTotalElements());
+    }
+
+    private FuncionarioRetornoDto mapFuncionarioRetornoDto(Funcionario funcionario) {
+        FuncionarioRetornoDto dto = new FuncionarioRetornoDto();
+
+        dto.setId(funcionario.getId());
+        dto.setUuidIdentificador(funcionario.getUuidIdentificador());
+        dto.setNumeroFuncionario(funcionario.getNumeroFuncionario());
+        dto.setNome(funcionario.getNome());
+        dto.setDataNascimento(funcionario.getDataNascimento());
+        dto.setCpf(funcionario.getCpf());
+        dto.setRg(funcionario.getRg());
+        dto.setEmail(funcionario.getEmail());
+        dto.setDdd(funcionario.getDdd());
+        dto.setTelefone(funcionario.getTelefone());
+        dto.setSituacao(funcionario.getSituacao());
+        dto.setEstadoCivil(funcionario.getEstadoCivil());
+
+        return dto;
     }
 }
