a90ea44a7c7d3731a50020346613daa13b53c8af
marciliodevjava <marciliodevjava@gmail.com>
2023-08-08 22:24:00 -0300

    Update FormatadorDeDados

--- Funcionario/src/main/java/br/com/funcionario/utils/FormatadorDeDados.java
+++ Funcionario/src/main/java/br/com/funcionario/utils/FormatadorDeDados.java
@@ -18,8 +18,18 @@ public class FormatadorDeDados {
         return cpf.trim();
     }
 
-    public String formatadorNome(String nome) {
-        return nome.trim();
+    public String formatadorNome(String nomeCompleto) {
+        String[] palavras = nomeCompleto.toLowerCase().split(" ");
+        StringBuilder resultado = new StringBuilder();
+
+        for (String palavra : palavras) {
+            if (!palavra.equals("de") || !palavra.equals("da")) {
+                resultado.append(Character.toUpperCase(palavra.charAt(0))).append(palavra.substring(1)).append(" ");
+            } else {
+                resultado.append(palavra).append(" ");
+            }
+        }
+        return resultado.toString().trim();
     }
 
     public String formatadorRg(String rg) {
@@ -55,5 +65,64 @@ public class FormatadorDeDados {
         String dataFormatada = formatarData.format(dataNascimento);
 
         return dataFormatada;
+    }
+
+    public String formatadorCepEndereco(String cep) {
+        cep = cep.replace("-", "");
+        return cep.trim();
+    }
+
+    public String formatadorLogradouroEndereco(String logradouro) {
+        logradouro = logradouro.replace("  ", " ");
+        logradouro = logradouro.replace(",", "");
+        logradouro = logradouro.replace(".", "");
+        logradouro = logradouro.replace("-", "");
+
+        String[] palavras = logradouro.toLowerCase().split(" ");
+        StringBuilder resultado = new StringBuilder();
+
+        for (String palavra : palavras) {
+            resultado.append(Character.toUpperCase(palavra.charAt(0))).append(palavra.substring(1)).append(" ");
+        }
+        return resultado.toString().trim();
+    }
+
+    public String formatadorNumeroEndereco(String numero) {
+        try {
+            Integer.parseInt(numero);
+            return String.valueOf(numero);
+        } catch (NumberFormatException e) {
+            return String.valueOf("S/N");
+        }
+    }
+
+    public String formatadorBairroEndereco(String bairro) {
+        String[] palavras = bairro.toLowerCase().split(" ");
+        StringBuilder resultado = new StringBuilder();
+
+        for (String palavra : palavras) {
+            resultado.append(Character.toUpperCase(palavra.charAt(0))).append(palavra.substring(1)).append(" ");
+        }
+        return resultado.toString().trim();
+    }
+
+    public String formatadorCidadeEndereco(String cidade) {
+        String primeiraLetra = cidade.substring(0,1).toUpperCase();
+        String restantePalavra = cidade.substring(1).toLowerCase();
+        String retorno = primeiraLetra + restantePalavra;
+
+        return retorno.trim();
+    }
+
+    public String formatadorUfEndereco(String uf) {
+        return uf.trim().toUpperCase();
+    }
+
+    public String formatadorPaisEndereco(String pais) {
+        String primeiraLetra = pais.substring(0,1).toUpperCase();
+        String restantePalavra = pais.substring(1).toLowerCase();
+        String retorno = primeiraLetra + restantePalavra;
+
+        return retorno.trim();
     }
 }
