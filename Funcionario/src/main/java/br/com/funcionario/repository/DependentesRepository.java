1b2dcdb0026c9c94f01d52d9f7c65ef930844e24
marcsilva <marcsilva@brasilseg.com.br>
2023-08-09 18:20:49 -0300

    Create DependentesRepository

--- Funcionario/src/main/java/br/com/funcionario/repository/DependentesRepository.java
+++ Funcionario/src/main/java/br/com/funcionario/repository/DependentesRepository.java
@@ -4,6 +4,10 @@ import br.com.funcionario.model.Dependentes;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DependentesRepository extends JpaRepository<Dependentes, Long> {
+
+    List<Dependentes> findByFuncionario(Long id);
 }
