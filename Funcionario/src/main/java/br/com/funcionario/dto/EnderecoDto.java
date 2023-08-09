6313383b84a01b1929787c3cba0c744c3804175b
marciliodevjava <marciliodevjava@gmail.com>
2023-08-08 22:24:24 -0300

    Update EnderecoDto

--- Funcionario/src/main/java/br/com/funcionario/dto/EnderecoDto.java
+++ Funcionario/src/main/java/br/com/funcionario/dto/EnderecoDto.java
@@ -1,4 +1,23 @@
 package br.com.funcionario.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class EnderecoDto {
+    @NotNull(message = "Campo cep inválido.")
+    private String cep;
+    @NotNull(message = "Campo logradouro inválido.")
+    private String logradouro;
+    @NotNull(message = "Campo numero inválido.")
+    private String numero;
+    @NotNull(message = "Campo bairro inválido.")
+    private String bairro;
+    @NotNull(message = "Campo cidade inválido.")
+    private String cidade;
+    @NotNull(message = "Campo uf inválido.")
+    private String uf;
+    private String pais;
 }
