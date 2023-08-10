774ed3cacd869b5efc35a5d3b40afffbd769f6a2
marciliodevjava <marciliodevjava@gmail.com>
2023-08-09 22:16:17 -0300

    Update EnderecoRepository

--- Funcionario/src/main/java/br/com/funcionario/repository/EnderecoRepository.java
+++ Funcionario/src/main/java/br/com/funcionario/repository/EnderecoRepository.java
@@ -1,9 +1,13 @@
 package br.com.funcionario.repository;
 
 import br.com.funcionario.model.Endereco;
+import br.com.funcionario.model.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+    Optional<Endereco> findByFuncionario(Funcionario funcionario);
 }
