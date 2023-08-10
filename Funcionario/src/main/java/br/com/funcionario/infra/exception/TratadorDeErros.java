34c0c6db6a9b931dbb025bfb8a455b2546a334b9
marciliodevjava <marciliodevjava@gmail.com>
2023-08-10 20:49:21 -0300

    Create TratadorDeErros

---
+++ Funcionario/src/main/java/br/com/funcionario/infra/exception/TratadorDeErros.java
@@ -0,0 +1,37 @@
+package br.com.funcionario.infra.exception;
+
+import br.com.funcionario.infra.exception.enuns.MensagemEnum;
+import br.com.funcionario.infra.exception.exception.ErroDeletarFuncionarioException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+    private final String projetoNome = "Api Funcionario";
+    @Value("${spring.application.name}")
+    private String projeto;
+    @Autowired
+    private HttpServletRequest request;
+
+    @ExceptionHandler(ErroDeletarFuncionarioException.class)
+    public ResponseEntity<ErroResponse> errorDeletarFuncionarioCmpleto(ErroDeletarFuncionarioException ex){
+        ErroResponse erroResponse = new ErroResponse();
+
+        erroResponse.setStatus(HttpStatus.OK.value());
+        erroResponse.setMensagem(Collections.singletonList(MensagemEnum.ERRO_DELETAR_FUNCIONARIO_COMPLETO.getMensagem()));
+        erroResponse.setTimestamp(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        erroResponse.setEndpoint(request.getRequestURI());
+        erroResponse.setProjeto(projetoNome);
+
+        return new ResponseEntity<>(erroResponse, HttpStatus.OK);
+    }
+}
