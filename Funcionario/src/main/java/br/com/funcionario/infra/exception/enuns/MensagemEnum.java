0109e135e73604772fa3b707491be1381a3b5f99
marciliodevjava <marciliodevjava@gmail.com>
2023-08-11 19:22:35 -0300

    Update MensagemEnum

--- Funcionario/src/main/java/br/com/funcionario/infra/exception/enuns/MensagemEnum.java
+++ Funcionario/src/main/java/br/com/funcionario/infra/exception/enuns/MensagemEnum.java
@@ -1,7 +1,9 @@
 package br.com.funcionario.infra.exception.enuns;
 
 public enum MensagemEnum {
-    ERRO_DELETAR_FUNCIONARIO_COMPLETO("Não existe esse funcionario na base de dados.");
+    ERRO_AO_ATUALIZAR_DEPENDENTE("Não possivel atualizar o dependentes informado."),
+    LIST_DEPENDENTE_NOT_FOUND("Não existe dependentes vinculados ao funcionario."),
+    ERRO_DELETAR_FUNCIONARIO_COMPLETO("Não existe esse funcionario.");
     private final String mensagem;
 
     MensagemEnum(String mensagem){
