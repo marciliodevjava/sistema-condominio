7a9026fbc44ac4808f789419b1bc95b8ce0cf581
marciliodevjava <marciliodevjava@gmail.com>
2023-08-11 19:23:02 -0300

    Update DependentesRepository

--- Funcionario/src/main/java/br/com/funcionario/repository/DependentesRepository.java
+++ Funcionario/src/main/java/br/com/funcionario/repository/DependentesRepository.java
@@ -3,7 +3,11 @@ package br.com.funcionario.repository;
 import br.com.funcionario.model.Dependentes;
 import br.com.funcionario.model.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +16,12 @@ import java.util.Optional;
 public interface DependentesRepository extends JpaRepository<Dependentes, Long> {
 
     List<Dependentes> findByFuncionario(Funcionario funcionario);
+    Dependentes findByUuidIdentificador(String uuid);
+    @Transactional
+    @Modifying
+    @Query(value = """
+            SELECT * FROM condominio.tb_dependentes WHERE id_funcionario = ?1
 
-    void deleteByFuncionario(Optional<Funcionario> funcionario);
+                        """, nativeQuery = true)
+    List<Dependentes> listDepentende(@PathVariable Long id);
 }
