8d0e384a0215db60f1745b4602bcf2d7a686fafe
marciliodevjava <marciliodevjava@gmail.com>
2023-08-14 10:50:24 -0300

    Create FuncionarioNaoExisteException

---
+++ Funcionario/src/main/java/br/com/funcionario/infra/exception/exception/FuncionarioNaoExisteException.java
@@ -0,0 +1,15 @@
+package br.com.funcionario.infra.exception.exception;
+
+public class FuncionarioNaoExisteException extends RuntimeException {
+    public FuncionarioNaoExisteException(){
+        super();
+    }
+
+    public FuncionarioNaoExisteException(String mensagem){
+        super(mensagem);
+    }
+
+    public FuncionarioNaoExisteException(String mensagem, Throwable couse){
+        super(mensagem, couse);
+    }
+}
