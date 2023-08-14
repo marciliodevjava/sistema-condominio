e548e07c7ab3620fd42088019dce398ec731da6e
marciliodevjava <marciliodevjava@gmail.com>
2023-08-14 13:12:01 -0300

    Create EnderecoRetornoDto

---
+++ Funcionario/src/main/java/br/com/funcionario/dto/EnderecoRetornoDto.java
@@ -0,0 +1,20 @@
+package br.com.funcionario.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class EnderecoRetornoDto {
+    private String uuidIdentificador;
+    private String cep;
+    private String logradouro;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String pais;
+}
