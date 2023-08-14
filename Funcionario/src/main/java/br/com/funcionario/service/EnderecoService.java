f656b6a19b3dc20f05158b4db80d70e67e95b84b
marciliodevjava <marciliodevjava@gmail.com>
2023-08-14 15:39:35 -0300

    Update EnderecoService

--- Funcionario/src/main/java/br/com/funcionario/service/EnderecoService.java
+++ Funcionario/src/main/java/br/com/funcionario/service/EnderecoService.java
@@ -1,7 +1,10 @@
 package br.com.funcionario.service;
 
+import br.com.funcionario.dto.EnderecoDto;
 import br.com.funcionario.dto.EnderecoRetornoDto;
 import br.com.funcionario.infra.exception.exception.EnderecoNaoExisteException;
+import br.com.funcionario.infra.exception.exception.EnderecoNaoFoiSalvoException;
+import br.com.funcionario.mapper.EnderecoMapper;
 import br.com.funcionario.model.Endereco;
 import br.com.funcionario.model.Funcionario;
 import br.com.funcionario.repository.EnderecoRepository;
@@ -23,16 +26,38 @@ public class EnderecoService {
     private ObjectMapper objectMapper;
     @Autowired
     private FormatadorDeDados formatadorDeDados;
+    @Autowired
+    private EnderecoMapper enderecoMapper;
 
     public EnderecoRetornoDto getEndereco(Long id) {
         Optional<Funcionario> funcionario = funcionarioRepository.findById(id);
 
-        if (funcionario.isPresent()){
+        if (funcionario.isPresent()) {
             Optional<Endereco> endereco = enderecoRepository.findByFuncionario(funcionario.get());
 
             return objectMapper.convertValue(endereco, EnderecoRetornoDto.class);
         }
 
         throw new EnderecoNaoExisteException();
+    }
+
+    public EnderecoRetornoDto getEnderecoUuid(String uuid) {
+        Optional<Endereco> endereco = enderecoRepository.findByUuidIdentificador(uuid);
+        if (endereco.isPresent()) {
+            return objectMapper.convertValue(endereco, EnderecoRetornoDto.class);
+        }
+        throw new EnderecoNaoExisteException();
+    }
+
+    public EnderecoRetornoDto cadastrarEndereco(Long id, EnderecoDto enderecoDto) {
+        Optional<Funcionario> funcionario = funcionarioRepository.findById(id);
+        if (funcionario != null){
+            Endereco endereco = enderecoMapper.mapearDependenteSalvar(funcionario.get(), enderecoDto);
+            endereco =enderecoRepository.save(endereco);
+
+            return objectMapper.convertValue(endereco, EnderecoRetornoDto.class);
+        }
+
+        throw new EnderecoNaoFoiSalvoException();
     }
 }
