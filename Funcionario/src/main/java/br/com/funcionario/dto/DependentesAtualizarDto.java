34103dd88724cbcb397cb782bc21e1c423fea122
marciliodevjava <marciliodevjava@gmail.com>
2023-08-14 10:50:39 -0300

    Create DependentesAtualizarDto

---
+++ Funcionario/src/main/java/br/com/funcionario/dto/DependentesAtualizarDto.java
@@ -0,0 +1,26 @@
+package br.com.funcionario.dto;
+
+import br.com.funcionario.model.enuns.GrauParentescoEnum;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
+
+@Getter
+@Setter
+public class DependentesAtualizarDto {
+    @NotNull(message = "Campo grauParentescoEnum inválido.")
+    private GrauParentescoEnum grauParentescoEnum;
+    @NotNull(message = "Campo nome inválido.")
+    private String nome;
+    @NotNull(message = "Campo dataNascimento inválido.")
+    private String dataNascimento;
+    @NotNull(message = "Campo cpf inválido.")
+    private String cpf;
+    @NotNull(message = "Campo rg inválido.")
+    private String rg;
+    @NotNull(message = "Campo ddd inválido.")
+    private String ddd;
+    @NotNull(message = "Campo telefone inválido.")
+    private String telefone;
+}
