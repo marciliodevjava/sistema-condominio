9327eb03cd1a6dc2cca8cc4188a4f167f9eaa585
marciliodevjava <marciliodevjava@gmail.com>
2023-08-14 15:40:20 -0300

    Update EnderecoMapper

--- Funcionario/src/main/java/br/com/funcionario/mapper/EnderecoMapper.java
+++ Funcionario/src/main/java/br/com/funcionario/mapper/EnderecoMapper.java
@@ -3,6 +3,7 @@ package br.com.funcionario.mapper;
 import br.com.funcionario.dto.AtualizarEnderecoDto;
 import br.com.funcionario.dto.EnderecoDto;
 import br.com.funcionario.model.Endereco;
+import br.com.funcionario.model.Funcionario;
 import br.com.funcionario.utils.FormatadorDeDados;
 import br.com.funcionario.utils.GeradorUuid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -52,6 +53,24 @@ public class EnderecoMapper {
                 end.setPais(dto.getPais() != null ? formatadorDeDados.formatadorPaisEndereco(dto.getPais()) : end.getPais());
                 return endereco;
             }
+        }
+        return null;
+    }
+
+    public Endereco mapearDependenteSalvar(Funcionario funcionario, EnderecoDto enderecoDto) {
+        Endereco endereco = new Endereco();
+        if (Objects.nonNull(enderecoDto)) {
+            endereco.setUuidIdentificador(geradorUuid.getIdentificadorUuid());
+            endereco.setCep(formatadorDeDados.formatadorCepEndereco(enderecoDto.getCep()));
+            endereco.setLogradouro(formatadorDeDados.formatadorLogradouroEndereco(enderecoDto.getLogradouro()));
+            endereco.setNumero(formatadorDeDados.formatadorNumeroEndereco(enderecoDto.getNumero()));
+            endereco.setBairro(formatadorDeDados.formatadorBairroEndereco(enderecoDto.getBairro()));
+            endereco.setCidade(formatadorDeDados.formatadorCidadeEndereco(enderecoDto.getCidade()));
+            endereco.setUf(formatadorDeDados.formatadorUfEndereco(enderecoDto.getUf()));
+            endereco.setPais(formatadorDeDados.formatadorPaisEndereco(enderecoDto.getPais()));
+            endereco.setFuncionario(funcionario);
+
+            return endereco;
         }
         return null;
     }
