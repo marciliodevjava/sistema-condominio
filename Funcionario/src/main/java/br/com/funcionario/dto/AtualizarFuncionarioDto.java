1382a568cb61c311d809a9a3d56234d45126a62a
marciliodevjava <marciliodevjava@gmail.com>
2023-08-15 14:54:35 -0300

    Update AtualizarFuncionarioDto

--- Funcionario/src/main/java/br/com/funcionario/dto/AtualizarFuncionarioDto.java
+++ Funcionario/src/main/java/br/com/funcionario/dto/AtualizarFuncionarioDto.java
@@ -1,6 +1,7 @@
 package br.com.funcionario.dto;
 
 import br.com.funcionario.model.enuns.EstadoCivilEnum;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.Email;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AtualizarFuncionarioDto {
     private String nome;
     @CPF
