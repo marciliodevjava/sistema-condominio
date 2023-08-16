9c13e79a7559889d66f6b5c1644fbdc2cfe5c7f8
marciliodevjava <marciliodevjava@gmail.com>
2023-08-16 14:53:37 -0300

    Create UuidRepository

---
+++ Morador/src/main/java/br/com/morador/repository/UuidRepository.java
@@ -0,0 +1,9 @@
+package br.com.morador.repository;
+
+import br.com.morador.domain.Uuid;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UuidRepository extends JpaRepository<Uuid, Long> {
+}
