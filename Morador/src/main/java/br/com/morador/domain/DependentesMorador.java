17b7a504122c6b7d77bc67e20533946579db3754
marciliodevjava <marciliodevjava@gmail.com>
2023-08-16 15:00:12 -0300

    Update DependentesMorador

--- Morador/src/main/java/br/com/morador/domain/DependentesMorador.java
+++ Morador/src/main/java/br/com/morador/domain/DependentesMorador.java
@@ -21,17 +21,26 @@ public class DependentesMorador implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "uuid_dependente", length = 100, nullable = false)
     private String uuidDependenteMorador;
+    @Column(name = "nome", length = 150, nullable = false)
     private String nome;
+    @Column(name = "cpf", unique = true, length = 11, nullable = false)
     private String cpf;
+    @Column(name = "rg", length = 10)
     private String rg;
+    @Column(name = "data_nascimento", nullable = false)
     private Date dataNascimento;
-    private String email;
+    @Column(name = "ddd_pais")
+    private String dddPais;
+    @Column(name = "ddd", length = 3)
     private String ddd;
+    @Column(name = "telefone", length = 9)
     private String telefone;
+    @Column(name = "genero", nullable = false, length = 12)
     @Enumerated(EnumType.STRING)
     private EnumSexo sexo;
     @ManyToOne(optional = false)
-    @JoinColumn(name = "id_apartamento")
-    private MoradorResponsavel responsavel;
+    @JoinColumn(name = "id_morador")
+    private MoradorResponsavel morador;
 }
