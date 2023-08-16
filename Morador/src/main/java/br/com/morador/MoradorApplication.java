778cf296015a084346ef6b6cc33a45e88a58fb6d
marciliodevjava <marciliodevjava@gmail.com>
2023-08-16 09:30:22 -0300

    Create API Morador

---
+++ Morador/src/main/java/br/com/morador/MoradorApplication.java
@@ -0,0 +1,13 @@
+package br.com.morador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoradorApplication.class, args);
+	}
+
+}
---
+++ Morador/src/test/java/br/com/morador/MoradorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.morador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
