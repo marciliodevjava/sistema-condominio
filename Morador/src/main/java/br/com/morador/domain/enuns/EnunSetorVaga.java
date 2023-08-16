3515b8af9314077a8830e89193808cd4103d0090
marciliodevjava <marciliodevjava@gmail.com>
2023-08-16 09:31:25 -0300

    Create EnunSetorVaga

---
+++ Morador/src/main/java/br/com/morador/domain/enuns/EnunSetorVaga.java
@@ -0,0 +1,24 @@
+package br.com.morador.domain.enuns;
+
+public enum EnunSetorVaga {
+    SETOR_A(1),
+    SETOR_B(2),
+    SETOR_C(3),
+    SETOR_D(4),
+    SETOR_E(5),
+    SETOR_F(6),
+    SETOR_SUB_SOLO_A(7),
+    SETOR_SUB_SOLO_B(8),
+    SETOR_SUB_SOLO_C(9),
+    SETOR_SUB_SOLO_D(10);
+
+    private final int valor;
+
+    EnunSetorVaga(int valor){
+        this.valor=valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+}
