70bcffd0ef8703d09fd084528f301ba9a8edb1e5
marciliodevjava <marciliodevjava@gmail.com>
2023-08-16 14:56:05 -0300

    Create ProprietarioRepository

---
+++ Morador/src/main/java/br/com/morador/repository/ProprietarioRepository.java
@@ -0,0 +1,9 @@
+package br.com.morador.repository;
+
+import br.com.morador.domain.Proprietarios;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProprietarioRepository extends JpaRepository<Proprietarios, Long> {
+}
