17234f5822c9bef7da962bd8985f9a4098016e9e
marciliodevjava <marciliodevjava@gmail.com>
2023-08-16 14:56:42 -0300

    Create EnumBloco

---
+++ Morador/src/main/java/br/com/morador/domain/enuns/EnumBloco.java
@@ -0,0 +1,27 @@
+package br.com.morador.domain.enuns;
+
+public enum EnumBloco {
+    BLOCO_A(1),
+
+    BLOCO_B(2),
+
+    BLOCO_C(3),
+
+    BLOCO_D(4),
+
+    BLOCO_F(5),
+
+    BLOCO_G(6),
+
+    BLOCO_H(7);
+
+    private final int valor;
+
+    EnumBloco(int valor) {
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+}
