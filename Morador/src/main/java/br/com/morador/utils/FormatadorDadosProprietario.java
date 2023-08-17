c2c6973b7739b02691b2d6ccd40b8eb7e007bbed
marciliodevjava <marciliodevjava@gmail.com>
2023-08-17 19:20:32 -0300

    Create FormatadorDadosProprietario

---
+++ Morador/src/main/java/br/com/morador/utils/FormatadorDadosProprietario.java
@@ -0,0 +1,8 @@
+package br.com.morador.utils;
+
+import br.com.morador.domain.enuns.EnumSexo;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FormatadorDadosProprietario {
+}
