be3e618153439a3a978c99927fa94f7660f48326
marciliodevjava <marciliodevjava@gmail.com>
2023-08-17 20:00:21 -0300

    Update FormatadorDadosProprietario

--- Morador/src/main/java/br/com/morador/utils/FormatadorDadosProprietario.java
+++ Morador/src/main/java/br/com/morador/utils/FormatadorDadosProprietario.java
@@ -3,6 +3,41 @@ package br.com.morador.utils;
 import br.com.morador.domain.enuns.EnumSexo;
 import org.springframework.stereotype.Component;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Component
 public class FormatadorDadosProprietario {
+    public String formatarNome(String nome) {
+        nome = nome.replace(".", "");
+        nome = nome.replace("-", "");
+        return nome.trim();
+    }
+
+    public String formatarCpf(String cpf) {
+        cpf = cpf.replace(".","");
+        cpf = cpf.replace("-","");
+        return cpf.trim();
+    }
+
+    public String formatarRg(String rg) {
+        rg = rg.replace(".","");
+        rg = rg.replace("-","");
+        rg = rg.replace("'\'","");
+        return rg.trim();
+    }
+
+    public Date formatarStringParaDate(String dataNascimento) {
+        String dataFormatar = "yyyy-MM-dd";
+        SimpleDateFormat formatarData = new SimpleDateFormat(dataFormatar);
+        Date data = null;
+        try {
+            data = formatarData.parse(dataNascimento);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        return data;
+    }
 }
