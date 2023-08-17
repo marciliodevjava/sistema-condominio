9016347b5fffbb60523cd99c041f2543ecffb58b
marciliodevjava <marciliodevjava@gmail.com>
2023-08-17 19:22:40 -0300

    Create FormatadorDadosDependentes

---
+++ Morador/src/main/java/br/com/morador/utils/FormatadorDadosDependentes.java
@@ -0,0 +1,8 @@
+package br.com.morador.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FormatadorDadosDependentes {
+}
