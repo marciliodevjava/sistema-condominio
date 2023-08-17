9f91b24738c9f97b1def010225c06e5578ada1f2
marciliodevjava <marciliodevjava@gmail.com>
2023-08-16 22:35:00 -0300

    Update VagasRepository

--- Morador/src/main/java/br/com/morador/repository/VagasRepository.java
+++ Morador/src/main/java/br/com/morador/repository/VagasRepository.java
@@ -2,8 +2,21 @@ package br.com.morador.repository;
 
 import br.com.morador.domain.Vagas;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Repository
 public interface VagasRepository extends JpaRepository<Vagas, Long> {
+
+    @Transactional
+    @Modifying
+    @Query(value = """ 
+            SELECT * FROM dbo.condominio.tb_vagas WHERE id = ?1 ORDER BY id DESC
+            """, nativeQuery = true)
+    Optional<Vagas> buscarVagas(@PathVariable Long id);
 }
