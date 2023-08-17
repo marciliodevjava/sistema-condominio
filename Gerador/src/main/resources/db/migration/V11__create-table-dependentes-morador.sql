fb30f0fca6204d8d1c2ee2486e37f73235d26c1e
marciliodevjava <marciliodevjava@gmail.com>
2023-08-16 22:35:18 -0300

    Create V11__create-table-dependentes-morador.sql

---
+++ Gerador/src/main/resources/db/migration/V11__create-table-dependentes-morador.sql
@@ -0,0 +1,17 @@
+CREATE TABLE tb_dependentes_morador
+(
+    id              BIGINT(20) not null AUTO_INCREMENT,
+    uuid_dependente VARCHAR(100) not null,
+    nome            VARCHAR(150) not null,
+    cpf             VARCHAR(11)  not null,
+    rg              VARCHAR(10),
+    data_nascimento DATE         not null,
+    ddd_pais        VARCHAR(7)   not null,
+    ddd             VARCHAR(3)   not null,
+    telefone        VARCHAR(9)   not null,
+    sexo            VARCHAR(12)  not null,
+    id_morador      BIGINT(20) not null,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_morador) REFERENCES tb_morador_responsavel (id)
+);
