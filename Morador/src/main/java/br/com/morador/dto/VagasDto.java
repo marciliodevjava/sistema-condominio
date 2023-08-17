34ef86bf67bb3edfaee465c352afa6af7f6f3ffa
marciliodevjava <marciliodevjava@gmail.com>
2023-08-17 19:18:31 -0300

    Create VagasDto

---
+++ Morador/src/main/java/br/com/morador/dto/VagasDto.java
@@ -0,0 +1,15 @@
+package br.com.morador.dto;
+
+import br.com.morador.domain.enuns.EnumSetorVaga;
+import br.com.morador.domain.enuns.EnumTiposVaga;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VagasDto {
+    private String uuidVagas;
+    private String numero;
+    private EnumSetorVaga setor;
+    private EnumTiposVaga tipo;
+}
