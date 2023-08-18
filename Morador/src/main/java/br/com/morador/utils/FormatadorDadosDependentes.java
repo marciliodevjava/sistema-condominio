d124d63e5aaef746bafdf157d2d79c5b8fb12627
marciliodevjava <marciliodevjava@gmail.com>
2023-08-18 08:54:50 -0300

    Update FormatadorDadosDependentes

--- Morador/src/main/java/br/com/morador/utils/FormatadorDadosDependentes.java
+++ Morador/src/main/java/br/com/morador/utils/FormatadorDadosDependentes.java
@@ -1,6 +1,6 @@
 package br.com.morador.utils;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import br.com.morador.domain.enuns.EnumSexo;
 import org.springframework.stereotype.Component;
 
 import java.text.ParseException;
@@ -10,21 +10,21 @@ import java.util.Date;
 @Component
 public class FormatadorDadosDependentes {
     public String formatarNome(String nome) {
-        nome = nome.replace(".","");
-        nome = nome.replace("-","");        
+        nome = nome.replace(".", "");
+        nome = nome.replace("-", "");
         return nome.trim();
     }
 
     public String formatarCpf(String cpf) {
-        cpf = cpf.replace(".","");
-        cpf = cpf.replace("-","");
+        cpf = cpf.replace(".", "");
+        cpf = cpf.replace("-", "");
         return cpf.trim();
     }
 
     public String formatarRg(String rg) {
-        rg = rg.replace(".","");
-        rg = rg.replace("-","");
-        rg = rg.replace("'\'","");
+        rg = rg.replace(".", "");
+        rg = rg.replace("-", "");
+        rg = rg.replace("'\'", "");
         return rg.trim();
     }
 
@@ -39,5 +39,37 @@ public class FormatadorDadosDependentes {
         }
 
         return data;
+    }
+
+    public String formatarDddPais(String dddPais) {
+        dddPais = dddPais.replace(",", "");
+        dddPais = dddPais.replace(".", "");
+        return dddPais.toUpperCase().trim();
+    }
+
+    public String formatarDdd(String ddd) {
+        if (ddd != null && Integer.valueOf(ddd) <= 3) {
+            return ddd.toUpperCase().trim();
+        }
+        return null;
+    }
+
+    public String formatarTelefone(String telefone) {
+        if (telefone != null && telefone instanceof String) {
+            telefone = telefone.replace(".", "");
+            telefone = telefone.replace("-", "").trim();
+            if (Integer.valueOf(telefone) <= 9) {
+                return telefone;
+            }
+            return "S/N";
+        }
+        return "N/I";
+    }
+
+    public EnumSexo formatarSexo(EnumSexo sexo) {
+        if (sexo != null && sexo instanceof EnumSexo) {
+            return sexo;
+        }
+        return EnumSexo.NAO_DEFINIDO;
     }
 }
