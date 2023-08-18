02a62957a89f52b149017b17ff6b2f6f4e232919
marciliodevjava <marciliodevjava@gmail.com>
2023-08-18 19:01:04 -0300

    Create MoradorResponsavelRetornoDto

---
+++ Morador/src/main/java/br/com/morador/dto/response/MoradorResponsavelRetornoDto.java
@@ -0,0 +1,26 @@
+package br.com.morador.dto.response;
+
+import br.com.morador.domain.DependentesMorador;
+import br.com.morador.domain.enuns.EnumSexo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MoradorResponsavelRetornoDto {
+    private String uuidMorador;
+    private String nome;
+    private String cpf;
+    private String rg;
+    private String dataNascimento;
+    private String dddPais;
+    private String ddd;
+    private String telefone;
+    private EnumSexo sexo;
+    private List<DependentesMoradorRetornoDto> dependentes = new ArrayList<>();
+}
