b96067ab99963088e386b50938703c42b505fad0
marciliodevjava <marciliodevjava@gmail.com>
2023-08-18 18:58:35 -0300

    Update ApartamentosMappper

--- Morador/src/main/java/br/com/morador/mapper/ApartamentosMappper.java
+++ Morador/src/main/java/br/com/morador/mapper/ApartamentosMappper.java
@@ -2,7 +2,8 @@ package br.com.morador.mapper;
 
 import br.com.morador.domain.Apartamentos;
 import br.com.morador.domain.Proprietario;
-import br.com.morador.dto.ApartamentosDto;
+import br.com.morador.dto.request.ApartamentosDto;
+import br.com.morador.dto.response.ApartamentosRetornoDto;
 import br.com.morador.utils.FormatadorDadosApartamento;
 import br.com.morador.utils.GeradorUuid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,6 +37,24 @@ public class ApartamentosMappper {
                 apt.setMorador(moradorResponsavelMapper.mapearMorador(apt, a.getMorador()));
                 apt.setVagas(vagasMapper.mapearVagas(apt, a.getVagas()));
             });
+        }
+        return null;
+    }
+
+    public List<ApartamentosRetornoDto> mapeiaApartamentoRetornoDto(List<Apartamentos> apartamento) {
+        List<ApartamentosRetornoDto> list = new ArrayList<>();
+        if (Objects.nonNull(apartamento)){
+            apartamento.forEach( a -> {
+                ApartamentosRetornoDto apt = new ApartamentosRetornoDto();
+                apt.setUuidApartamento(a.getUuidApartamento());
+                apt.setNumero(a.getNumero());
+                apt.setBloco(a.getBloco());
+                apt.setAndar(a.getAndar());
+                apt.setMorador(moradorResponsavelMapper.mapeiaMoradorResponsavelRetornoDto(a.getMorador()));
+                apt.setVagas(vagasMapper.mapeiaVagasRetornoDto(a.getVagas()));
+                list.add(apt);
+            });
+            return list;
         }
         return null;
     }
