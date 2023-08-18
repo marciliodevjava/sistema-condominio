40217c8484ae9705cbd556ad7ff5058c37e76a50
marciliodevjava <marciliodevjava@gmail.com>
2023-08-18 19:11:42 -0300

    Create ApartamentosRetornoDto

---
+++ Morador/src/main/java/br/com/morador/dto/response/ApartamentosRetornoDto.java
@@ -0,0 +1,22 @@
+package br.com.morador.dto.response;
+
+import br.com.morador.domain.enuns.EnumAndar;
+import br.com.morador.domain.enuns.EnumBloco;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ApartamentosRetornoDto {
+    private String uuidApartamento;
+    private Integer numero;
+    private EnumBloco bloco;
+    private EnumAndar andar;
+    private MoradorResponsavelRetornoDto morador;
+    private List<VagasRetornoDto> vagas = new ArrayList<>();
+}
