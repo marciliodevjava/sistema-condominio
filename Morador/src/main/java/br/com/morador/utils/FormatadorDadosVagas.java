5a0b58c986ee0bdd57666bbd4bbba58f1575af7f
marciliodevjava <marciliodevjava@gmail.com>
2023-08-18 08:54:00 -0300

    Update FormatadorDadosVagas

--- Morador/src/main/java/br/com/morador/utils/FormatadorDadosVagas.java
+++ Morador/src/main/java/br/com/morador/utils/FormatadorDadosVagas.java
@@ -1,7 +1,28 @@
 package br.com.morador.utils;
 
+import br.com.morador.domain.enuns.EnumSetorVaga;
+import br.com.morador.domain.enuns.EnumTiposVaga;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FormatadorDadosVagas {
+    public String numero(String numero) {
+        numero = numero.replace(".", "");
+        numero = numero.replace("-", "");
+        return numero.trim();
+    }
+
+    public EnumSetorVaga setor(EnumSetorVaga setor) {
+        if (setor instanceof  EnumSetorVaga){
+            return setor;
+        }
+        return EnumSetorVaga.SETOR_INDEFINIDO;
+    }
+
+    public EnumTiposVaga tipo(EnumTiposVaga tipo) {
+        if (tipo instanceof EnumTiposVaga){
+            return tipo;
+        }
+        return EnumTiposVaga.S_CAR;
+    }
 }
