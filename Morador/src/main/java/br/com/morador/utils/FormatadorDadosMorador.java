8aad7826d1778ef5694ebc8d2297a3aa0ad0176c
marciliodevjava <marciliodevjava@gmail.com>
2023-08-18 18:57:11 -0300

    Update FormatadorDadosMorador

--- Morador/src/main/java/br/com/morador/utils/FormatadorDadosMorador.java
+++ Morador/src/main/java/br/com/morador/utils/FormatadorDadosMorador.java
@@ -72,4 +72,17 @@ public class FormatadorDadosMorador {
         }
         return EnumSexo.NAO_DEFINIDO;
     }
+
+    public String formatadorDateParaString(Date dataNascimento) {
+        String dataFormatar = "yyyy-MM-dd";
+        SimpleDateFormat formatarData = new SimpleDateFormat(dataFormatar);
+        Date data = null;
+        try {
+            data = formatarData.parse(String.valueOf(dataNascimento));
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        return String.valueOf(data);
+    }
 }
