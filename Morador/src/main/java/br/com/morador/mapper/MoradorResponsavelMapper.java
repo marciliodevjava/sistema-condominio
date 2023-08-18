1c2b772297a00f9746d9ffe98a6e4bfc3ea3f621
marciliodevjava <marciliodevjava@gmail.com>
2023-08-18 08:53:13 -0300

    Update MoradorResponsavelMapper

--- Morador/src/main/java/br/com/morador/mapper/MoradorResponsavelMapper.java
+++ Morador/src/main/java/br/com/morador/mapper/MoradorResponsavelMapper.java
@@ -2,16 +2,12 @@ package br.com.morador.mapper;
 
 import br.com.morador.domain.Apartamentos;
 import br.com.morador.domain.MoradorResponsavel;
-import br.com.morador.domain.Vagas;
 import br.com.morador.dto.MoradorResponsavelDto;
-import br.com.morador.dto.VagasDto;
 import br.com.morador.utils.FormatadorDadosMorador;
 import br.com.morador.utils.GeradorUuid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 @Component
@@ -25,7 +21,7 @@ public class MoradorResponsavelMapper {
 
     public MoradorResponsavel mapearMorador(Apartamentos apt, MoradorResponsavelDto morador) {
         MoradorResponsavel moradorResponsavel = new MoradorResponsavel();
-        if (Objects.nonNull(morador)){
+        if (Objects.nonNull(morador)) {
             moradorResponsavel.setUuidMorador(geradorUuid.gerarUuid());
             moradorResponsavel.setNome(formatadorDadosMorador.formatadorNome(morador.getNome()));
             moradorResponsavel.setCpf(formatadorDadosMorador.formatadorCpf(morador.getCpf()));
@@ -34,6 +30,7 @@ public class MoradorResponsavelMapper {
             moradorResponsavel.setDddPais(formatadorDadosMorador.formatadorDddPais(morador.getDddPais()));
             moradorResponsavel.setDdd(formatadorDadosMorador.formatadorDdd(morador.getDdd()));
             moradorResponsavel.setTelefone(formatadorDadosMorador.formatadorTelefone(morador.getTelefone()));
+            moradorResponsavel.setSexo(formatadorDadosMorador.formatadorSexo(morador.getSexo()));
             moradorResponsavel.setDependentes(dependentesMapper.mapearDependentes(moradorResponsavel, morador.getDependentes()));
             moradorResponsavel.setApartamento(apt);
             return moradorResponsavel;
