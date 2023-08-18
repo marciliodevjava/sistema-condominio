fbceeff09964d31beae24b027aa3c3f4f2218d75
marciliodevjava <marciliodevjava@gmail.com>
2023-08-18 08:51:42 -0300

    Update ProprietarioService

--- Morador/src/main/java/br/com/morador/service/ProprietarioService.java
+++ Morador/src/main/java/br/com/morador/service/ProprietarioService.java
@@ -1,21 +1,12 @@
 package br.com.morador.service;
 
-import br.com.morador.domain.Apartamentos;
-import br.com.morador.domain.MoradorResponsavel;
 import br.com.morador.domain.Proprietario;
-import br.com.morador.domain.Vagas;
 import br.com.morador.dto.*;
 import br.com.morador.mapper.ProprietarioMapper;
 import br.com.morador.repository.ProprietarioRepository;
-import br.com.morador.utils.FormatadorDadosMorador;
-import br.com.morador.utils.FormatadorDadosProprietario;
-import br.com.morador.utils.FormatadorDadosVagas;
-import br.com.morador.utils.GeradorUuid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -24,6 +15,7 @@ public class ProprietarioService {
     private ProprietarioRepository proprietarioRepository;
     @Autowired
     private ProprietarioMapper proprietarioMapper;
+
     public ProprietarioRetornoDto salvaProprietario(ProprietarioDto dto) {
         if (Objects.nonNull(dto)) {
             Proprietario proprietario = proprietarioMapper.mapearProprietaSimples(dto);
@@ -31,4 +23,10 @@ public class ProprietarioService {
         return null;
     }
 
+    public ProprietarioRetornoDto salvaProprietarioCompleto(ProprietarioDto dto) {
+        if (Objects.nonNull(dto)) {
+            Proprietario proprietario = proprietarioMapper.mapearProprietaSimples(dto);
+        }
+        return null;
+    }
 }
