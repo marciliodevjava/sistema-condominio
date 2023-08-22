9eef5295ab0496d3ca440fa15e9c4d91f678a27b
marciliodevjava <marciliodevjava@gmail.com>
2023-08-22 09:11:01 -0300

    Update ApartamentosRepository

--- Morador/src/main/java/br/com/morador/repository/ApartamentosRepository.java
+++ Morador/src/main/java/br/com/morador/repository/ApartamentosRepository.java
@@ -1,9 +1,9 @@
 package br.com.morador.repository;
 
-import br.com.morador.domain.Apartamentos;
+import br.com.morador.domain.Apartamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ApartamentosRepository extends JpaRepository<Apartamentos, Long> {
+public interface ApartamentosRepository extends JpaRepository<Apartamento, Long> {
 }
