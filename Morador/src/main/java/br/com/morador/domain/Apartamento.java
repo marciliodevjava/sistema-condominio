db0d16aa87638227fb0dccc99e949b45df774938
marciliodevjava <marciliodevjava@gmail.com>
2023-08-22 09:11:28 -0300

    Update Apartamentos

--- Morador/src/main/java/br/com/morador/domain/Apartamento.java
+++ Morador/src/main/java/br/com/morador/domain/Apartamento.java
@@ -13,37 +13,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "tb_apartamentos")
+@Table(name = "tb_apartamento")
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
 @Getter
 @EqualsAndHashCode
-public class Apartamentos implements Serializable {
+public class Apartamento implements Serializable {
     @Serial
     private static final long serialVersionUID= 10L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "uuid_apartamento", length = 100, nullable = false)
+    @Column(name = "uuid_apartamento", length = 100)
     private String uuidApartamento;
-    @Column(name = "numero", length = 5, nullable = false)
+    @Column(name = "numero", length = 5)
     private Integer numero;
-    @Column(name = "bloco", length = 7, nullable = false)
+    @Column(name = "bloco", length = 7)
     @Enumerated(EnumType.STRING)
     private EnumBloco bloco;
-    @Column(name = "andar", length = 7, nullable = false)
+    @Column(name = "andar", length = 7)
     @Enumerated(EnumType.STRING)
     private EnumAndar andar;
     @Setter(onMethod = @__({@JsonProperty}))
     @Getter(onMethod = @__({@JsonIgnore}))
-    @OneToOne(cascade = CascadeType.ALL, mappedBy = "apartamento", fetch = FetchType.LAZY)
+    @OneToOne(mappedBy = "apartamento", cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
     private MoradorResponsavel morador;
     @Setter(onMethod = @__({@JsonProperty}))
     @Getter(onMethod = @__({@JsonIgnore}))
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "apartamento", fetch = FetchType.LAZY)
     private List<Vagas> vagas = new ArrayList<>();
+
     @ManyToOne(optional = false)
-    @JoinColumn(name = "id_proprietario", nullable = false)
+    @JoinColumn(name = "id_proprietario")
     private Proprietario proprietario;
 }
