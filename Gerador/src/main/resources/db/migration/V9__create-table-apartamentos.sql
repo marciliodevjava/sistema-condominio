aee35a3cc67a8849beb2518158ca3182ddd9b534
marciliodevjava <marciliodevjava@gmail.com>
2023-08-22 09:11:42 -0300

    Update V9__create-table-apartamentos.sql

--- Gerador/src/main/resources/db/migration/V9__create-table-apartamentos.sql
+++ Gerador/src/main/resources/db/migration/V9__create-table-apartamentos.sql
@@ -1,12 +1,11 @@
-CREATE TABLE tb_apartamentos
+CREATE TABLE tb_apartamento
 (
     id               bigint(20) not null AUTO_INCREMENT,
     uuid_apartamento VARCHAR(100) not null,
     numero           INTEGER(5) not null,
     bloco            VARCHAR(7)   not null,
-    rg               VARCHAR(10)  not null,
     andar            VARCHAR(7)   not null,
-    id_proprietario  BIGINT(20) not null,
+    id_proprietario  BIGINT(20),
 
     PRIMARY KEY (id),
     FOREIGN KEY (id_proprietario) REFERENCES tb_proprietario(id)
