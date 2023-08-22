23f38d09c8da98a727a34deec859112d26f8e72c
marciliodevjava <marciliodevjava@gmail.com>
2023-08-22 09:11:19 -0300

    Update ApartamentosManager

--- Morador/src/main/java/br/com/morador/manager/ApartamentosManager.java
+++ Morador/src/main/java/br/com/morador/manager/ApartamentosManager.java
@@ -1,12 +1,11 @@
 package br.com.morador.manager;
 
-import br.com.morador.domain.Apartamentos;
+import br.com.morador.domain.Apartamento;
 import br.com.morador.exception.ErroSalvarApartamentoException;
 import br.com.morador.repository.ApartamentosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -15,11 +14,10 @@ public class ApartamentosManager {
     @Autowired
     private ApartamentosRepository apartamentosRepository;
 
-    public List<Apartamentos> salvarListApartamentos(List<Apartamentos> apartamento) {
-        List<Apartamentos> listApt = new ArrayList<>(apartamento);
+    public List<Apartamento> salvarListApartamentos(List<Apartamento> apartamento) {
         if (Objects.nonNull(apartamento)) {
-            listApt = apartamentosRepository.saveAll(listApt);
-            return listApt;
+            apartamento = apartamentosRepository.saveAll(apartamento);
+            return apartamento;
         }
         throw new ErroSalvarApartamentoException();
     }
