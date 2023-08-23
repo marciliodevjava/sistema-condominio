6190b5dd1cf7b5ca7a926f1c128628212bddf609
marciliodevjava <marciliodevjava@gmail.com>
2023-08-23 20:47:46 -0300

    Update ProprietarioManager

--- Morador/src/main/java/br/com/morador/manager/ProprietarioManager.java
+++ Morador/src/main/java/br/com/morador/manager/ProprietarioManager.java
@@ -1,12 +1,14 @@
 package br.com.morador.manager;
 
 import br.com.morador.domain.Proprietario;
+import br.com.morador.dto.ErroBuscarProprietarioException;
 import br.com.morador.exception.ErroSalvarProprietarioException;
 import br.com.morador.repository.ProprietarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
+import java.util.Optional;
 
 @Component
 public class ProprietarioManager {
@@ -19,5 +21,13 @@ public class ProprietarioManager {
             return proprietario;
         }
         throw new ErroSalvarProprietarioException();
+    }
+
+    public Optional<Proprietario> buscarProprietario(String uuid) {
+        if (Objects.nonNull(uuid)) {
+            Optional<Proprietario> proprietario = proprietarioRepository.findByUuidProprietario(uuid);
+            return proprietario;
+        }
+        throw new ErroBuscarProprietarioException();
     }
 }
