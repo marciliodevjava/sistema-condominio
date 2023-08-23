e1d44e70153e019cb2a0d00352300d5707fffa78
marciliodevjava <marciliodevjava@gmail.com>
2023-08-23 17:02:28 -0300

    Update V12__create-table-vagas.sql

--- Gerador/src/main/resources/db/migration/V12__create-table-vagas.sql
+++ Gerador/src/main/resources/db/migration/V12__create-table-vagas.sql
@@ -5,8 +5,8 @@ CREATE TABLE tb_vagas
     numero         VARCHAR(150) not null,
     setor          VARCHAR(11)  not null,
     tipo           VARCHAR(10)  not null,
-    id_apartamento BIGINT(20),
+    id_morador_responsavel      BIGINT(20),
 
     PRIMARY KEY (id),
-    FOREIGN KEY (id_apartamento) REFERENCES tb_apartamento(id)
+    FOREIGN KEY (id_morador_responsavel) REFERENCES tb_morador_responsavel (id)
 );
