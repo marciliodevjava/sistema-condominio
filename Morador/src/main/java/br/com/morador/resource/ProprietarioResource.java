04bf05682ac8ad17d62ef481374e30e00f972787
marciliodevjava <marciliodevjava@gmail.com>
2023-08-23 17:03:08 -0300

    Update ProprietarioResource

--- Morador/src/main/java/br/com/morador/resource/ProprietarioResource.java
+++ Morador/src/main/java/br/com/morador/resource/ProprietarioResource.java
@@ -5,10 +5,7 @@ import br.com.morador.dto.response.ProprietarioRetornoDto;
 import br.com.morador.service.ProprietarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -32,5 +29,11 @@ public class ProprietarioResource {
         ProprietarioRetornoDto retorno = proprietarioService.salvaProprietarioCompleto(dto);
         URI uri = uriBuild.path("/buscar/{uuid}").buildAndExpand(retorno.getUuidProprietario()).toUri();
         return ResponseEntity.created(uri).body(retorno);
+    }
+
+    @GetMapping("/buscar/{uuid}")
+    public ResponseEntity<ProprietarioRetornoDto> getProprietário(@PathVariable String uuid){
+        ProprietarioRetornoDto dto = proprietarioService.buscarProponente(uuid);
+        return ResponseEntity.ok(dto);
     }
 }
