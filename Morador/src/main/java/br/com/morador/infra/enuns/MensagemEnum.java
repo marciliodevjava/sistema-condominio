a73ec0a36f7379131aa0f237d7e6b4b39628b8a8
marciliodevjava <marciliodevjava@gmail.com>
2023-08-24 12:48:57 -0300

    Update MensagemEnum

--- Morador/src/main/java/br/com/morador/infra/enuns/MensagemEnum.java
+++ Morador/src/main/java/br/com/morador/infra/enuns/MensagemEnum.java
@@ -1,6 +1,8 @@
 package br.com.morador.infra.enuns;
 
 public enum MensagemEnum {
+    ERRO_SALVAR_APARTAMENTO("Não foi possivel salva os dados do Apartamento."),
+    ERRO_BUSCAR_PROPRIETARIO("Não foi possivel buscar o proprietário."),
     ERRO_SALVAR_PROPRIETARIO_SIMPLES("Não foi possivel salvo o proprietário.");
     private final String mensagem;
     MensagemEnum(String mensagem){
