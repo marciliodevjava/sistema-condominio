dbf6999dc2aef8de5c3314251b7f54ca3a71604e
marciliodevjava <marciliodevjava@gmail.com>
2023-08-24 12:57:29 -0300

    Update ApartamentosDto

--- Morador/src/main/java/br/com/morador/dto/request/ApartamentosDto.java
+++ Morador/src/main/java/br/com/morador/dto/request/ApartamentosDto.java
@@ -2,15 +2,14 @@ package br.com.morador.dto.request;
 
 import br.com.morador.domain.enuns.EnumAndar;
 import br.com.morador.domain.enuns.EnumBloco;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ApartamentosDto {
     @NotNull(message = "numero invalido")
     private Integer numero;
