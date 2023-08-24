cb6ab947ecba2dc3a89768b165e78645984cd46d
marciliodevjava <marciliodevjava@gmail.com>
2023-08-24 12:57:09 -0300

    Update DependentesMoradorUpdateDto

--- Morador/src/main/java/br/com/morador/dto/request/DependentesMoradorUpdateDto.java
+++ Morador/src/main/java/br/com/morador/dto/request/DependentesMoradorUpdateDto.java
@@ -1,12 +1,14 @@
 package br.com.morador.dto.request;
 
 import br.com.morador.domain.enuns.EnumSexo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.br.CPF;
 
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class DependentesMoradorUpdateDto {
     private String uuidDependenteMorador;
     private String nome;
