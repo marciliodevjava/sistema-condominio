ccffcf955fb5f7a6e0d35157c49270230a0b2979
marciliodevjava <marciliodevjava@gmail.com>
2023-08-24 12:48:27 -0300

    Update MoradorResponsavelMapper

--- Morador/src/main/java/br/com/morador/mapper/MoradorResponsavelMapper.java
+++ Morador/src/main/java/br/com/morador/mapper/MoradorResponsavelMapper.java
@@ -3,6 +3,7 @@ package br.com.morador.mapper;
 import br.com.morador.domain.Apartamento;
 import br.com.morador.domain.MoradorResponsavel;
 import br.com.morador.dto.request.MoradorResponsavelDto;
+import br.com.morador.dto.request.MoradorResponsavelUpdateDto;
 import br.com.morador.dto.response.MoradorResponsavelRetornoDto;
 import br.com.morador.utils.FormatadorDadosMorador;
 import br.com.morador.utils.GeradorUuid;
@@ -44,7 +45,7 @@ public class MoradorResponsavelMapper {
 
     public MoradorResponsavelRetornoDto mapeiaMoradorResponsavelRetornoDto(MoradorResponsavel morador) {
         MoradorResponsavelRetornoDto dto = new MoradorResponsavelRetornoDto();
-        if (Objects.nonNull(morador)){
+        if (Objects.nonNull(morador)) {
             dto.setUuidMorador(morador.getUuidMorador());
             dto.setNome(morador.getNome());
             dto.setCpf(morador.getCpf());
@@ -56,6 +57,23 @@ public class MoradorResponsavelMapper {
             dto.setDependentes(dependentesMapper.mapeiaDependentesRetornoDto(morador.getDependentes()));
             dto.setVagas(vagasMapper.mapeiaVagasRetornoDto(morador.getVagas()));
             return dto;
+        }
+        return null;
+    }
+
+    public MoradorResponsavel mapearMoradorUpdate(Apartamento pro, MoradorResponsavel morador, MoradorResponsavelUpdateDto mru) {
+        if (Objects.nonNull(morador) && Objects.nonNull(mru)) {
+            morador.setNome(!mru.getNome().equals(null) && !mru.getNome().isEmpty() ? formatadorDadosMorador.formatadorNome(mru.getNome()) : morador.getNome());
+            morador.setCpf(!mru.getCpf().equals(null) && !mru.getCpf().isEmpty() ? formatadorDadosMorador.formatadorCpf(mru.getCpf()) : morador.getCpf());
+            morador.setRg(!mru.getRg().equals(null) && !mru.getRg().isEmpty() ? formatadorDadosMorador.formatadorRg(mru.getRg()) : morador.getCpf());
+            morador.setDataNascimento(!mru.getDataNascimento().equals(null) && !mru.getDataNascimento().isEmpty() ? formatadorDadosMorador.formatadorStringParaDate(mru.getDataNascimento()) : morador.getDataNascimento());
+            morador.setDddPais(!mru.getDddPais().equals(null) && !mru.getDddPais().isEmpty() ? formatadorDadosMorador.formatadorDddPais(mru.getDddPais()) : morador.getDddPais());
+            morador.setDdd(!mru.getDdd().equals(null) && !mru.getDdd().isEmpty() ? formatadorDadosMorador.formatadorDdd(mru.getDdd()) : morador.getDdd());
+            morador.setTelefone(!mru.getTelefone().equals(null) && !mru.getTelefone().isEmpty() ? formatadorDadosMorador.formatadorTelefone(mru.getTelefone()) : morador.getTelefone());
+            morador.setDependentes(!mru.getDependentes().equals(null) ? dependentesMapper.mapearDependentesUpdate(morador, morador.getDependentes(), mru.getDependentes()) : morador.getDependentes());
+            morador.setVagas(!mru.getVagas().equals(null) ? vagasMapper.mapearVagasUpdate(morador, morador.getVagas(), mru.getVagas()) : morador.getVagas());
+
+            return morador;
         }
         return null;
     }
