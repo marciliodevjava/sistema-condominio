b07bf35ce169b13124b87ac36b477f2ac624cd50
marciliodevjava <marciliodevjava@gmail.com>
2023-08-24 12:56:28 -0300

    Update VagasDto

--- Morador/src/main/java/br/com/morador/dto/request/VagasDto.java
+++ Morador/src/main/java/br/com/morador/dto/request/VagasDto.java
@@ -2,11 +2,13 @@ package br.com.morador.dto.request;
 
 import br.com.morador.domain.enuns.EnumSetorVaga;
 import br.com.morador.domain.enuns.EnumTiposVaga;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class VagasDto {
     private String numero;
     private EnumSetorVaga setor;
