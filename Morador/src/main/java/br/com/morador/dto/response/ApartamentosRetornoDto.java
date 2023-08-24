ede64390369ee0d6966d03d591f6de92b3f48b50
marciliodevjava <marciliodevjava@gmail.com>
2023-08-24 13:02:44 -0300

    Update ApartamentosRetornoDto

--- Morador/src/main/java/br/com/morador/dto/response/ApartamentosRetornoDto.java
+++ Morador/src/main/java/br/com/morador/dto/response/ApartamentosRetornoDto.java
@@ -6,9 +6,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Getter
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
