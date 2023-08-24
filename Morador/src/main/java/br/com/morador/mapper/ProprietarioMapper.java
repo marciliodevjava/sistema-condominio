a2d00bf8be71b0d4374b74bba7005b18f70ef606
marciliodevjava <marciliodevjava@gmail.com>
2023-08-24 12:48:03 -0300

    Update ProprietarioMapper

--- Morador/src/main/java/br/com/morador/mapper/ProprietarioMapper.java
+++ Morador/src/main/java/br/com/morador/mapper/ProprietarioMapper.java
@@ -2,6 +2,7 @@ package br.com.morador.mapper;
 
 import br.com.morador.domain.Proprietario;
 import br.com.morador.dto.request.ProprietarioDto;
+import br.com.morador.dto.request.ProprietarioUpdateDto;
 import br.com.morador.dto.response.ProprietarioRetornoDto;
 import br.com.morador.utils.FormatadorDadosProprietario;
 import br.com.morador.utils.GeradorUuid;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
+import java.util.Optional;
 
 @Component
 public class ProprietarioMapper {
@@ -53,5 +55,24 @@ public class ProprietarioMapper {
             return dto;
         }
         return null;
+    }
+
+    public Proprietario atualizarProprietario(Optional<Proprietario> proprietario,
+                                              ProprietarioUpdateDto dto) {
+        Proprietario pro = proprietario.get();
+        if (proprietario != null && Objects.nonNull(dto)) {
+            pro.setNome(!dto.getNome().equals(null) && !dto.getNome().isEmpty() ? formatadorDadosProprietario.formatarNome(dto.getNome()) : pro.getNome());
+            pro.setCpf(!dto.getCpf().equals(null) && !dto.getCpf().isEmpty() ? formatadorDadosProprietario.formatarCpf(dto.getCpf()) : pro.getCpf());
+            pro.setRg(!dto.getRg().equals(null) && !dto.getRg().isEmpty() ? formatadorDadosProprietario.formatarRg(dto.getRg()) : pro.getRg());
+            pro.setDataNascimento(!dto.getDataNascimento().equals(null) && !dto.getDataNascimento().isEmpty() ? formatadorDadosProprietario.formatarStringParaDate(dto.getDataNascimento()) : pro.getDataNascimento());
+            pro.setDddPais(!dto.getDddPais().equals(null) && !dto.getDdd().isEmpty() ? formatadorDadosProprietario.formatarDddPais(dto.getDddPais()) : pro.getDddPais());
+            pro.setDdd(!dto.getDdd().equals(null) && !dto.getDdd().isEmpty() ? formatadorDadosProprietario.formatarDdd(dto.getDdd()) : pro.getDdd());
+            pro.setTelefone(!dto.getTelefone().equals(null) && !dto.getTelefone().isEmpty() ? formatadorDadosProprietario.formatarTelefone(dto.getTelefone()) : pro.getTelefone());
+            pro.setSexo(!dto.getSexo().equals(null) && !dto.getSexo().toString().isEmpty() ? formatadorDadosProprietario.formatarSexo(dto.getSexo()) : pro.getSexo());
+            pro.setApartamento(!dto.getApartamento().equals(null) ? apartamentosMappper.mepearUpdateApartamento(pro, pro.getApartamento(), dto.getApartamento()) : pro.getApartamento());
+
+            return pro;
+        }
+        return pro;
     }
 }
