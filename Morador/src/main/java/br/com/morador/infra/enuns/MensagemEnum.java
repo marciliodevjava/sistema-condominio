d50846fbd622568ce48be1bc0f4ae76b94a507d1
marciliodevjava <marciliodevjava@gmail.com>
2023-08-25 19:59:39 -0300

    Update MensagemEnum

--- Morador/src/main/java/br/com/morador/infra/enuns/MensagemEnum.java
+++ Morador/src/main/java/br/com/morador/infra/enuns/MensagemEnum.java
@@ -4,7 +4,8 @@ public enum MensagemEnum {
     ERRO_SALVAR_APARTAMENTO("Não foi possivel salva os dados do Apartamento."),
     ERRO_BUSCAR_PROPRIETARIO("Não foi possivel buscar o proprietário."),
     ERRO_SALVAR_PROPRIETARIO_SIMPLES("Não foi possivel salvo o proprietário."),
-    ERRO_DELETAR_PROPRIETARIO("Erro ao deletar proprietario");
+    ERRO_DELETAR_PROPRIETARIO("Erro ao deletar proprietario"),
+    ERRO_SALVAR_DEPENDENTE("Não foi possivel salvar os dados do Dependente.");
     private final String mensagem;
     MensagemEnum(String mensagem){
         this.mensagem = mensagem;
