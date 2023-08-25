9acafebf089388a880b9f222bdeb3af6452fad88
marciliodevjava <marciliodevjava@gmail.com>
2023-08-25 20:00:17 -0300

    Update DependentesManager

--- Morador/src/main/java/br/com/morador/manager/DependentesManager.java
+++ Morador/src/main/java/br/com/morador/manager/DependentesManager.java
@@ -1,10 +1,13 @@
 package br.com.morador.manager;
 
+import br.com.morador.domain.DependentesMorador;
 import br.com.morador.query.DependentesMoradorQuery;
 import br.com.morador.repository.DependentesMoradorRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Component
 public class DependentesManager {
@@ -14,4 +17,12 @@ public class DependentesManager {
     private DependentesMoradorQuery dependentesMoradorQuery;
     @Autowired
     private HttpServletRequest request;
+
+    public DependentesMorador salvar(DependentesMorador dependentesMorador) {
+        if (Objects.nonNull(dependentesMorador)){
+            dependentesMorador = dependentesMoradorRepository.save(dependentesMorador);
+            return dependentesMorador;
+        }
+        return null;
+    }
 }
