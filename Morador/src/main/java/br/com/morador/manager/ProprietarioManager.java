feff410cb118e1db8cb1709584db0a67ec7e20e1
marcsilva <marcsilva@brasilseg.com.br>
2023-08-25 17:58:51 -0300

    Update ProprietarioManager

--- Morador/src/main/java/br/com/morador/manager/ProprietarioManager.java
+++ Morador/src/main/java/br/com/morador/manager/ProprietarioManager.java
@@ -1,12 +1,18 @@
 package br.com.morador.manager;
 
 import br.com.morador.domain.Proprietario;
+import br.com.morador.dto.ProprietarioDeletadoDto;
 import br.com.morador.exception.ErroBuscarProprietarioException;
 import br.com.morador.exception.ErroSalvarProprietarioException;
+import br.com.morador.query.ProprietarioQuery;
 import br.com.morador.repository.ProprietarioRepository;
+import br.com.morador.utils.FormatadorDadosProprietario;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -14,6 +20,12 @@ import java.util.Optional;
 public class ProprietarioManager {
     @Autowired
     private ProprietarioRepository proprietarioRepository;
+    @Autowired
+    private ProprietarioQuery proprietarioQuery;
+    @Autowired
+    private HttpServletRequest request;
+    @Autowired
+    private FormatadorDadosProprietario formatadorDadosProprietario;
 
     public Proprietario salvarProprietario(Proprietario proprietario) {
         if (Objects.nonNull(proprietario)) {
@@ -29,5 +41,16 @@ public class ProprietarioManager {
             return proprietario;
         }
         throw new ErroBuscarProprietarioException();
+    }
+
+    public ProprietarioDeletadoDto deletaProprietario(Optional<Proprietario> proprietario) {
+        Proprietario pro = proprietario.get();
+        if (Objects.nonNull(proprietario)){
+            boolean suceso = proprietarioQuery.deletar(pro);
+            if (suceso){
+                return new ProprietarioDeletadoDto("Proprietário deletado com Sucesso!", LocalDate.now(), 200, request.getRequestURI());
+            }
+        }
+        return null;
     }
 }
