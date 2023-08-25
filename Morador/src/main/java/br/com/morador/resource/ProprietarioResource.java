6ff7e9e24ba5cfe5a0fa630bf92e23af94dba1d8
marcsilva <marcsilva@brasilseg.com.br>
2023-08-25 17:58:25 -0300

    Update ProprietarioResource

--- Morador/src/main/java/br/com/morador/resource/ProprietarioResource.java
+++ Morador/src/main/java/br/com/morador/resource/ProprietarioResource.java
@@ -1,15 +1,18 @@
 package br.com.morador.resource;
 
+import br.com.morador.dto.ProprietarioDeletadoDto;
 import br.com.morador.dto.request.ProprietarioUpdateDto;
 import br.com.morador.dto.request.ProprietarioDto;
 import br.com.morador.dto.response.ProprietarioRetornoDto;
 import br.com.morador.service.ProprietarioService;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/proprietario")
@@ -42,5 +45,11 @@ public class ProprietarioResource {
     public ResponseEntity<ProprietarioRetornoDto> atualizar(@PathVariable String uuid, @RequestBody ProprietarioUpdateDto proprietarioDto) {
         ProprietarioRetornoDto dto = proprietarioService.atualizarProprietario(uuid, proprietarioDto);
         return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping("/deletar/{uuid}")
+    public ResponseEntity<ProprietarioDeletadoDto> deletar(@PathVariable @NotNull String uuid){
+        ProprietarioDeletadoDto deletado = proprietarioService.deletar(uuid);
+        return ResponseEntity.ok(deletado);
     }
 }
