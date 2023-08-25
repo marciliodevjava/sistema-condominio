a32a78be5fce9d2e2231977860b8ab5f7b9249ac
marcsilva <marcsilva@brasilseg.com.br>
2023-08-25 17:59:49 -0300

    Update ApartamentosRepository

--- Morador/src/main/java/br/com/morador/repository/ApartamentosRepository.java
+++ Morador/src/main/java/br/com/morador/repository/ApartamentosRepository.java
@@ -2,8 +2,18 @@ package br.com.morador.repository;
 
 import br.com.morador.domain.Apartamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Repository
 public interface ApartamentosRepository extends JpaRepository<Apartamento, Long> {
+    @Modifying
+    @Transactional
+    @Query(value = """
+            DELETE FROM condominio.tb_apartamento WHERE id = ?1
+            """, nativeQuery = true)
+    void deleteApartamento(@PathVariable Long id);
 }
