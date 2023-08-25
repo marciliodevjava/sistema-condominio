661fddc3b976422de2690becbc63d68ed070c71d
marcsilva <marcsilva@brasilseg.com.br>
2023-08-25 17:59:01 -0300

    Update MoradorResponsavelRepository

--- Morador/src/main/java/br/com/morador/repository/MoradorResponsavelRepository.java
+++ Morador/src/main/java/br/com/morador/repository/MoradorResponsavelRepository.java
@@ -2,8 +2,18 @@ package br.com.morador.repository;
 
 import br.com.morador.domain.MoradorResponsavel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Repository
 public interface MoradorResponsavelRepository extends JpaRepository<MoradorResponsavel, Long> {
+    @Transactional
+    @Modifying
+    @Query(value = """
+            DELETE FROM condominio.tb_morador_responsavel WHERE id = ?1
+            """, nativeQuery = true)
+    void deleteMorador(@PathVariable Long id);
 }
