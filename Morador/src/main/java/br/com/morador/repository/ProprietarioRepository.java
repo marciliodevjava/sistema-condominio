01c2681299cd4b8ec231d12922bc494c8b258c93
marcsilva <marcsilva@brasilseg.com.br>
2023-08-25 17:58:42 -0300

    Update ProprietarioRepository

--- Morador/src/main/java/br/com/morador/repository/ProprietarioRepository.java
+++ Morador/src/main/java/br/com/morador/repository/ProprietarioRepository.java
@@ -2,7 +2,10 @@ package br.com.morador.repository;
 
 import br.com.morador.domain.Proprietario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -10,4 +13,11 @@ import java.util.Optional;
 public interface ProprietarioRepository extends JpaRepository<Proprietario, Long> {
 
     Optional<Proprietario> findByUuidProprietario(String uuid);
+
+    @Transactional
+    @Modifying
+    @Query(value = """
+            DELETE FROM condominio.tb_proprietario WHERE id = ?1
+            """, nativeQuery = true)
+    void deleteProprietario(Long id);
 }
