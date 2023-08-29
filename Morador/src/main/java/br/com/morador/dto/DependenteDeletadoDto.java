2388f7f9ce15a52722dffd623b9334bcd0615184
marciliodevjava <marciliodevjava@gmail.com>
2023-08-29 16:59:40 -0300

    Create DependenteDeletadoDto

---
+++ Morador/src/main/java/br/com/morador/dto/DependenteDeletadoDto.java
@@ -0,0 +1,20 @@
+package br.com.morador.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class DependenteDeletadoDto {
+
+    private String mensagem;
+    private LocalDate data;
+    private Integer code;
+    private String endpoint;
+}
