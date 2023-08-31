a7d60e83a446ed1fae08eb0b7b59469f6fd5be9c
marciliodevjava <marciliodevjava@gmail.com>
2023-08-31 20:55:29 -0300

    Create BatchConfig

---
+++ email/src/main/java/br/com/email/domain/EmailDomain.java
@@ -0,0 +1,31 @@
+package br.com.email.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_email")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailDomain implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long emailId;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    private LocalDateTime sendDateEmail;
+    private StatusEmail statusEmail;
+}
